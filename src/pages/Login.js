0d86826d27321db43b78d2b2b39394426db489e0
vanderson-henrique <vhbenedito@gmail.com>
2021-01-26 20:10:46 -0300

    Requisitos 2,3,4,5,6,7,8 - Tela de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-function Login() {
-  return <p>AQUI É O LOGIN</p>;
+function Login({ history }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDisable, setIsDisable] = useState(true);
+
+  const verifyData = () => {
+    const six = 6;
+    const regex = /\S+@\S+\.\S+/;
+    if (regex.test(email) && password.length > six) setIsDisable(false);
+  };
+
+  useEffect(() => {
+    verifyData();
+  }, [email, password]);
+
+  const handleClick = () => {
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/comidas');
+  };
+
+  return (
+    <form>
+      <input
+        type="email"
+        name="email"
+        value={ email }
+        placeholder="Digite seu Email"
+        data-testid="email-input"
+        onChange={ ({ target }) => setEmail(target.value) }
+        autoCorrect="off"
+      />
+      <input
+        type="password"
+        name="password"
+        value={ password }
+        placeholder="Digite sua Senha"
+        data-testid="password-input"
+        onChange={ ({ target }) => setPassword(target.value) }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ isDisable }
+        onClick={ handleClick }
+      >
+        Entrar
+      </button>
+    </form>
+  );
 }
+
+Login.propTypes = {
+  history: PropTypes.func.isRequired,
+};
 
 export default Login;
