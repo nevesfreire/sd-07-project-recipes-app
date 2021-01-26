baa980a5d829d446942d74fc4178642e1e745846
Pablo <pablopessanh@gmail>
2021-01-26 20:18:32 -0300

    login fixes

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Login = () => (
+  <div />
+);
 
 export default Login;
