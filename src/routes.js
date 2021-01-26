b804764d4e97f612ab3611a12823bf29a16fe0ad
Hercules Gabriel <51159478+herculesgabriel@users.noreply.github.com>
2021-01-26 17:23:19 -0300

    Update src/routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 
 function Routes() {
