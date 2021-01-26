fdfcc13a2d7c05a2f7c1095ec725d10fe610863d
Flavio Oliveira <flavsoliver@gmail.com>
2021-01-26 20:47:17 -0300

    requisito 01 - com implementação da estrutura de pastas, rotas e criação do Context e Provider

---
+++ src/routes/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from '../pages/Login';
+
+const Routes = () => (
+  <Switch>
+    <Route
+      exact
+      path="/"
+      component={ Login }
+    />
+
+  </Switch>
+);
+
+export default Routes;
