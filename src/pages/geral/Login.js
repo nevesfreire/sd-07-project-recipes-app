9090fbcf01812cc545134f9dd85fbd23e78ac36e
Breno Alves <brenoalv3ss@gmail.com>
2021-01-26 17:34:09 -0300

    Tela Login

--- src/pages/geral/Login.js
+++ src/pages/geral/Login.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target: { value, name } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleClick() {
+    const { email } = this.state;
+    const { history } = this.props;
+    localStorage.setItem('mealsToken', '1');
+    localStorage.setItem('cocktailsToken ', '1');
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/comidas');
+  }
+
   render() {
+    const { email, password } = this.state;
+    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\.com$/;
+    const regexPassword = 6;
     return (
       <div>
         <h1>
@@ -11,15 +40,23 @@ class Login extends Component {
           type="text"
           data-testid="email-input"
           placeholder="E-Mail"
+          value={ email }
+          name="email"
+          onChange={ (event) => this.handleChange(event) }
         />
         <input
-          type="text"
+          type="password"
           data-testid="password-input"
           placeholder="Senha"
+          value={ password }
+          name="password"
+          onChange={ (event) => this.handleChange(event) }
         />
         <button
           type="button"
           data-testid="login-submit-btn"
+          disabled={ !regexEmail.test(email) || password.length < regexPassword }
+          onClick={ () => this.handleClick() }
         >
           Entrar
         </button>
@@ -27,5 +64,9 @@ class Login extends Component {
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.objectOf({ push: PropTypes.func.isRequired }).isRequired,
+};
 
 export default Login;
