dd328d266fe63ed3fc0a8653b417eeacdd9c96cb
Julio Kauer <juliokauer@gmail.com>
2021-01-27 16:47:01 -0300

    Com Bootstrap

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,85 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from './App';
+import Login from './pages/Login';
 
-test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('2 - Crie todos os elementos que devem respeitar os atributos descritos no protótipo para a tela de login', () => {
+  it('Tem os data-testids email-input, password-input e login-submit-btn', () => {
+    const { getByTestId } = render(<Login />);
+    const emailInput = getByTestId('email-input');
+    const passwordInput = getByTestId('password-input');
+    const submitButton = getByTestId('login-submit-btn');
+    
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+
+  });
+});
+
+describe('3 - Desenvolva a tela de maneira que a pessoa deve conseguir escrever seu email no input de email', () => {
+  it('É possível escrever o email', () => {
+    const { getByTestId } = render(<Login />);
+    const emailInput = getByTestId('email-input');
+
+    fireEvent.change(emailInput, { target: { value: 'email@mail.com' } });
+    expect(emailInput.value).toBe('email@mail.com');
+  });
+});
+
+describe('4 - Desenvolva a tela de maneira que a pessoa deve conseguir escrever sua senha no input de senha', () => {
+  it('É possível escrever a senha', () => {
+    const { getByTestId } = render(<Login />);
+    const passwordInput = getByTestId('password-input');
+    
+    fireEvent.change(passwordInput, { target: { value: '1234567' } });
+    expect(passwordInput.value).toBe('1234567');
+  });
+});
+
+describe('5 - Desenvolva a tela de maneira que o formulário só seja válido após um email válido e uma senha de mais de 6 caracteres serem preenchidos', () => {
+  it('O botão deve estar desativado se o email for inválido', () => {
+    const { getByTestId } = render(<Login />);
+    const emailInput = getByTestId('email-input');
+    const passwordInput = getByTestId('password-input');
+    const submitButton = getByTestId('login-submit-btn');
+
+    expect(submitButton.disabled).toBeTruthy();
+
+    fireEvent.change(emailInput, { target: { value: 'email@mail' } });
+    fireEvent.change(passwordInput, { target: { value: '1234567' } });
+    expect(submitButton.disabled).toBeTruthy();
+
+    fireEvent.change(emailInput, { target: { value: 'email.com' } });
+
+    expect(submitButton.disabled).toBeTruthy();
+  });
+
+  it('O botão deve estar desativado se a senha deve tiver 6 caracteres ou menos', () => {
+    const { getByTestId } = render(<Login />);
+    const emailInput = getByTestId('email-input');
+    const passwordInput = getByTestId('password-input');
+    const submitButton = getByTestId('login-submit-btn');
+
+    expect(submitButton.disabled).toBeTruthy();
+
+    fireEvent.change(emailInput, { target: { value: 'email@mail.com' } });
+    fireEvent.change(passwordInput, { target: { value: '123456' } });
+
+    expect(submitButton.disabled).toBeTruthy();
+  });
+
+  it('O botão deve estar ativado se o email e a senha forem válidos', () => {
+    const { getByTestId } = render(<Login />);
+    const emailInput = getByTestId('email-input');
+    const passwordInput = getByTestId('password-input');
+    const submitButton = getByTestId('login-submit-btn');
+
+    expect(submitButton.disabled).toBeTruthy();
+
+    fireEvent.change(emailInput, { target: { value: 'email@mail.com' } });
+    fireEvent.change(passwordInput, { target: { value: '1234567' } });
+
+    expect(submitButton.disabled).toBeFalsy();
+  });
 });
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  const isEmailValid = () => {
+    const format = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.match(format)) {
+      console.log('OI');
+      return true;
+    }
+    return false;
+  };
+
+  const isPasswordValid = () => {
+    const MIN_LENGTH = 7;
+    if (password.length >= MIN_LENGTH) return true;
+    return false;
+  };
+  const isInputsValid = () => {
+    if (isEmailValid() && isPasswordValid()) {
+      setDisabled(false);
+    }
+    setDisabled(true);
+  };
+
   return (
-    <p>Login</p>
+    <Form>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ ({ target: { value } }) => {
+            setEmail(value);
+            isInputsValid();
+          } }
+        />
+      </Form.Group>
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ ({ target: { value } }) => {
+            setPassword(value);
+            isInputsValid();
+          } }
+        />
+      </Form.Group>
+      <Button
+        variant="primary"
+        type="submit"
+        data-testid="login-submit-btn"
+        disabled={ disabled }
+      >
+        Submit
+      </Button>
+    </Form>
   );
 }
 
