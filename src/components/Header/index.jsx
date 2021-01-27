b874492ff662d58b9e4d6159ae960ac4d60c2d92
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-01-27 00:05:40 -0300

    criando componente Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import profileIcon from '../../images/profileIcon.svg';
+import searchIcon from '../../images/searchIcon.svg';
+
+export default function Header() {
+  return (
+    <div>
+      <div>
+        <img
+          src={ profileIcon }
+          alt="profile icon"
+          data-testid="profile-top-btn"
+        />
+      </div>
+      <h1 data-testid="page-title">Title</h1>
+      <div>
+        <img
+          src={ searchIcon }
+          alt="search icon"
+          data-testid="search-top-btn"
+        />
+      </div>
+    </div>
+  )
+}
