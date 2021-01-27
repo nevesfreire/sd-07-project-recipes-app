7ff5bc746fa4ba4acbde1743ebf35a93325c81f8
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2021-01-27 14:48:34 -0300

    Alteraçoes para os requisitos 2 a 8 - Login Page

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+const USER_EMAIL = 'USER_EMAIL';
+
+export default {
+  USER_EMAIL,
+};
+
+export const getEmail = (email) => ({
+  type: USER_EMAIL,
+  email,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,93 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getEmail } from '../actions';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.verifyEmailName = this.verifyEmailName.bind(this);
+  }
+
+  handleSubmit() {
+    const { sendEmail, history } = this.props;
+    const { email } = this.state;
+    sendEmail(email);
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    const emailInfo = JSON.stringify({ email });
+    localStorage.setItem('use', emailInfo);
+    history.push('/comidas');
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  verifyEmailName() {
+    const { email, password } = this.state;
+    const minLength = 6;
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email) && password.length >= minLength;
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <div>
-        Login Page
+        <form>
+          <label htmlFor="email">
+            Email
+            <input
+              placeholder="Email"
+              data-testid="email-input"
+              value={ email }
+              onChange={ this.handleChange }
+              name="email"
+            />
+          </label>
+          <label htmlFor="password">
+            Senha
+            <input
+              placeholder="Nome"
+              data-testid="password-input"
+              type="text"
+              value={ password }
+              onChange={ this.handleChange }
+              name="password"
+            />
+          </label>
+        </form>
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          disabled={ !this.verifyEmailName() }
+          onClick={ this.handleSubmit }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  sendEmail: (email) => dispatch(getEmail(email)),
+});
+
+Login.propTypes = {
+  sendEmail: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
