3726c21dbfdc83ac2c79b96af34c7961ba0b7895
Quintiliano Dalete <quintilianodalete@gmail.com>
2021-01-27 13:54:41 -0300

    feat: implemented the redux ducks, pages and components pages structure

---
+++ src/store/ducks/initialState.js
@@ -0,0 +1,13 @@
+// Neste arquivo sera armazenado TODO o estado da aplicacao.
+
+export default {
+  login: {
+    // Estados referentes a tela de login
+  },
+  receitasDeBebidas: {
+    // Estados referentes a receitas de bebidas
+  },
+  receitasDeComidas: {
+    // Estados referentes a receitas de comidas
+  },
+};
