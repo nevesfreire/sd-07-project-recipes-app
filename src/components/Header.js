0b9e6d02528377d034b2f915042d42a0df0f3fa3
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-01-27 16:06:56 -0300

    refatorando o Header

---
+++ src/components/Header.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
+import '../css/food.css';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.changeDisplayInput = this.changeDisplayInput.bind(this);
+    this.state = {
+      showInputSearch: false,
+    };
+  }
+
+  changeDisplayInput() {
+    const { showInputSearch } = this.state;
+    if (showInputSearch) {
+      this.setState({ showInputSearch: false });
+    } else this.setState({ showInputSearch: true });
+  }
+
+  render() {
+    const { history } = this.props;
+    const { showInputSearch } = this.state;
+    console.log(history);
+    return (
+      <div>
+        <header className="header-food-container">
+          <button
+            type="button"
+            data-testid="profile-top-btn"
+            onClick={ () => history.push('/perfil') }
+          >
+            <img src={ profileIcon } alt="profileIcon" />
+          </button>
+
+          <h1 data-testid="page-title">Comidas</h1>
+          <button
+            type="button"
+            data-testid="search-top-btn"
+            onClick={ this.changeDisplayInput }
+          >
+            <img src={ searchIcon } alt="searchIcon" />
+          </button>
+        </header>
+        <section>
+          {showInputSearch && (
+            <div className="search-food-container">
+              <input
+                name="searchFood"
+                data-testid="search-input"
+                className="search-food-input"
+                placeholder="Buscar Receita"
+              />
+            </div>
+          )}
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Header;
+
+Header.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
--- src/pages/Food.js
+++ src/pages/Food.js
@@ -1,54 +1,23 @@
 import React, { Component } from 'react';
-import profileIcon from '../images/profileIcon.svg'
-import searchIcon from '../images/searchIcon.svg'
+import PropTypes from 'prop-types';
+import Header from '../components/Header';
 import '../css/food.css';
 
 class Food extends Component {
-  constructor(props) {
-    super(props);
-    this.changeDisplayInput = this.changeDisplayInput.bind(this);
-  }
-
-  changeDisplayInput() {
-    const inputSearch = document.querySelector('.search-food-input');
-    // console.log(inputSearch.style.display);
-    if (inputSearch.style.display === 'block') {
-      return inputSearch.style.display = 'none';
-    }
-    return inputSearch.style.display = 'block';
-  }
-
   render() {
     const { history } = this.props;
     return (
       <div>
-        <header className="header-food-container">
-          <img
-            data-testid="profile-top-btn"
-            src={ profileIcon }
-            alt="profileIcon"
-            onClick={ () => history.push('/perfil') }
-          />
-          <h1 data-testid="page-title">FOOD</h1>
-          <img
-            data-testid="search-top-btn"
-            src={ searchIcon }
-            alt="searchIcon"
-            onClick={ this.changeDisplayInput }
-          />
-        </header>
-        <section>
-          <div className="search-food-container">
-            <input 
-              name="searchFood"
-              className="search-food-input"
-              placeholder="Buscar Receita"
-            />
-          </div>
-        </section>
+        <Header history={ history } />
       </div>
     );
   }
 }
 
 export default Food;
+
+Food.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
