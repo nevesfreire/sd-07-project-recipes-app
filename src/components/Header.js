136a87e6aab3202602b8d55632c803b68e276a90
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-01-27 18:59:12 -0300

    requisito 10

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,41 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
+  const history = useHistory();
+  const titleGeneration = () => {
+    const path = history.location.pathname;
+    const titleArray = path.split('/');
+    const zero = 0;
+    const two = 2;
+    const titleMap = titleArray.map((palavra) => (palavra.length > zero
+      ? palavra.replace(palavra.charAt(zero), palavra.charAt(zero).toUpperCase())
+      : ''));
+    const title = titleMap.length > two
+      ? `${titleMap[1]} ${titleMap[two]}`
+      : `${titleMap[1]}`;
+    return title;
+  };
+
   return (
     <div>
-
-      <button
-        type="button"
+      <img
+        src={ profileIcon }
+        alt="profile icon"
         data-testid="profile-top-btn"
-        id="profileBtn"
-      >
-        Profile
-      </button>
+      />
 
-      <h1 data-testid="page-title">Header</h1>
+      <h1 data-testid="page-title">
+        { titleGeneration() }
+      </h1>
 
-      <button
-        type="button"
+      <img
+        src={ searchIcon }
+        alt="profile icon"
         data-testid="search-top-btn"
-        id="searchBtn"
-      >
-        Search
-      </button>
-
+      />
     </div>
   );
 }
