c47eb706ed23ddfac727695f348f304b791d88bb
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2021-01-27 15:07:39 -0300

    ajustes de importacao

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
-import Comidas from './Pages/Perfil';
+import Comidas from './Pages/Comidas/Comidas';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
