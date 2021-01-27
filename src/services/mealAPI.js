46a229f23c6571ea9f6aa0a012faefb2c709bfa9
Elisa <elisatcamillo@gmail.com>
2021-01-27 20:10:20 -0300

    initial structure

---
+++ src/services/mealAPI.js
@@ -0,0 +1,8 @@
+// const api = 'https://www.themealdb.com/api/json/v1/1/';
+// const byIngredient = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`;
+// const byName = `https://www.themealdb.com/api/json/v1/1/search.php?s=${nome}`;
+// const byFirstLetter = `https://www.themealdb.com/api/json/v1/1/search.php?f=${primeiraletra}`;
+// const allMeals = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+// const listCategories = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+// const byId = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+// const randomMeal = 'https://www.themealdb.com/api/json/v1/1/random.php';
