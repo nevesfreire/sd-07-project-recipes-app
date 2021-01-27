40d607aaf2e0fef4e30c1d3dc38eb5df62b14859
Flavio Oliveira <flavsoliver@gmail.com>
2021-01-27 16:28:48 -0300

    requisito 05 -  validação de email e senha

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import RecipesContext from '../../context/RecipesContext';
 import './Login.css';
 
 export default function Login() {
+  const {
+    login, setLogin, disabled, setDisabled,
+  } = useContext(RecipesContext);
+
+  const validateEmailAndPassword = () => {
+    const emailValidation = (/[A-Z0-9]{1,}@[A-Z0-9]{2,}\.[A-Z0-9]{2,}/i)
+      .test(login.email);
+    const minPass = 5;
+
+    const validation = emailValidation && login.password.length > minPass
+      ? setDisabled(false)
+      : setDisabled(true);
+    return validation;
+  };
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setLogin({ ...login, [name]: value });
+    validateEmailAndPassword();
+  };
+
   return (
     <div className="login-container">
       <label htmlFor="email">
@@ -10,6 +32,7 @@ export default function Login() {
           type="email"
           name="email"
           data-testid="email-input"
+          onChange={ (e) => handleChange(e) }
         />
       </label>
       <label htmlFor="password">
@@ -18,11 +41,13 @@ export default function Login() {
           type="password"
           name="password"
           data-testid="password-input"
+          onChange={ (e) => handleChange(e) }
         />
       </label>
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ disabled }
       >
         Entrar
       </button>
