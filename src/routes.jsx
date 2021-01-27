14b8b26eb5217f4a0a328afcbf090194fdfbc933
Luiz Mariz <lfsmariz@gmail.com>
2021-01-27 14:48:59 -0300

    add route comidas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ComidaPage from './Pages/ComidaPage';
 import LoginPage from './Pages/LoginPage';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={ LoginPage } />
+      <Route exact path="/comidas" component={ ComidaPage } />
       {/* TODO PAGES */}
       {/* <Route exact path="/comidas" component={  } />
       <Route exact path="/bebidas" component={  } />
