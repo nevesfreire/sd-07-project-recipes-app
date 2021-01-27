5b54fe4e601b1b7ba7a61e74cbbb210d4ef2b3e6
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2021-01-27 14:41:33 -0300

    Estruturando Header da Tela de Comidas - 27/01/2021

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
+import Food from './pages/Food';
+import Profile from './pages/Profile';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/comidas" component={ Food } />
+      <Route path="/perfil" component={ Profile } />
     </Switch>
   );
 }
