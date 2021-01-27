d90fe95e4075edc5e3f84374466e11ed91644484
Dhiego Dias <ddias003@gmail.com>
2021-01-27 19:46:39 -0300

    Fix do Lint

--- src/pages/food.js
+++ src/pages/food.js
@@ -1,12 +1,12 @@
 import React, { } from 'react';
 import Header from '../components/Header';
-import Footer from '../components/Footer'
+import Footer from '../components/Footer';
 
 function Food() {
   return (
     <div>
-    <Header />
-    <Footer />
+      <Header />
+      <Footer />
     </div>
   );
 }
