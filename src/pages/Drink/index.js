275c47547a8efb7603c411510c8eb3fa9d8cce44
murilopereira006 <muriloedu006@gmail.com>
2021-01-27 19:29:05 -0300

    header

--- src/pages/Drink/index.js
+++ src/pages/Drink/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Header from '../../Components/Header';
 
 const Drink = () => (
-  <div />
+  <div>
+    <Header>Bebidas</Header>
+    Dink PG
+  </div>
 );
 
 export default Drink;
--- src/pages/Food/index.js
+++ src/pages/Food/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Header from '../../Components/Header';
 
 const Food = () => (
-  <div />
+  <div>
+    <Header test="page-title">Comidas</Header>
+    Food PG
+  </div>
 );
 
 export default Food;
