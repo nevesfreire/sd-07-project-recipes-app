388b8c4e25723618c6c7f6003f8c8973f6db344d
Rodolfo <rodolfojr94@gmail.com>
2021-01-27 17:51:02 -0300

    Criado useFetchApi

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useLocalStorage } from './useLocalStorage';
 export { default as useValideEmailAndPassword } from './useValideEmailAndPassword';
+export { default as useFetchApi } from './useFetchApi';
