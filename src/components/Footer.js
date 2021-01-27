c4acb3ba72978f2b487284b1bdd4b89f8a1a4125
Dhiego Dias <ddias003@gmail.com>
2021-01-27 19:39:55 -0300

    Requisitos 19, 20, 22 e 23

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import mealIcon from '../images/mealIcon.svg';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+
+function Footer() {
+  return (
+    <footer data-testid="footer" className="footer">
+      <Link to="/comidas">
+        <img
+          data-testid="food-bottom-btn"
+          src={ mealIcon }
+          alt="meal-icon"
+        />
+      </Link>
+      <Link to="/bebidas">
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="drink-icon"
+        />
+      </Link>
+      <Link to="/explorar">
+        <img
+          data-testid="explore-bottom-btn"
+          src={ exploreIcon }
+          alt="explore-icon"
+        />
+      </Link>
+    </footer>
+  );
+}
+
+export default Footer;
