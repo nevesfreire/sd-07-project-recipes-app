6fc122ccdd21d7883c16252c35b7639fd915cd6a
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-01-27 00:23:17 -0300

    adicionado importacao das paginas explorar bebidas e comidas

--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header } from '../../components';
 
-export default function Expore() {
+export default function Explore() {
   return (
     <Header />
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import Login from './Login';
 import MainFood from './MainFood';
 import MainDrink from './MainDrink';
+import Explore from './Explore';
+import ExploreFoods from './ExploreFoods';
+import ExploreDrinks from './ExploreDrinks';
 
-export { Login, MainFood, MainDrink };
+export {
+  Login,
+  MainFood,
+  MainDrink,
+  Explore,
+  ExploreFoods,
+  ExploreDrinks,
+};
