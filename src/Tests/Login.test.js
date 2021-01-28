87e5efbef7877b4f87322284cbfab30d765f199b
Fabio Camargos <fabiocmmcm@gmail.com>
2021-01-28 18:06:59 -0300

    Update erro do tests 2

--- src/Tests/Login.test.js
+++ src/Tests/Login.test.js
@@ -6,16 +6,15 @@ import Login from '../pages/Login';
 
 const myEmail = 'email-input';
 const myPassword = 'password-input';
-const myButton = 'login-submit-btn'
+const myButton = 'login-submit-btn';
 
 afterEach(cleanup);
 
 describe('Teste página de Login', () => {
-  const { getByTestId, history } = renderWithRouter(<Login />);
+  const { getByTestId } = renderWithRouter(<Login />);
   const email = getByTestId(myEmail);
   const senha = getByTestId(myPassword);
-  const button = getByTestId(myButton); 
-
+  const button = getByTestId(myButton);
 
   it('Crie os elementos que devem respeitar os atributos descritos no protótipo', () => {
     expect(email).toBeInTheDocument();
