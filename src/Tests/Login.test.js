1b2372989badbef3317189944ce1c166980b5529
Fabio Camargos <fabiocmmcm@gmail.com>
2021-01-28 15:17:10 -0300

    testeLogin cc 2

--- src/Tests/Login.test.js
+++ src/Tests/Login.test.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import {cleanup} from '@testing-library/react'
+import { cleanup } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import Login from '../pages/Login';
-import App from '../App';
 
 describe('Teste página de Login', () => {
   const { getByTestId, history } = renderWithRouter(<Login />);
   const email = getByTestId('email-input');
   const senha = getByTestId('password-input');
-  const button = getByTestId('login-submit-btn');
-  
-  afterEach(cleanup)
+  const button = getByTestId('login-submit-btn'); 
+  afterEach(cleanup);
 
   it('Crie os elementos que devem respeitar os atributos descritos no protótipo', () => {
     expect(email).toBeInTheDocument();
