90634871cdba532a6b0d94db2576b09e5e519b83
Luiz Mariz <lfsmariz@gmail.com>
2021-01-28 17:08:11 -0300

    create BebidasPage

---
+++ src/Pages/BebidasPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MainScreen from '../../Components/MainScreen';
+
+const BebidasPage = () => (
+  <div>
+    <h1>Bebidas</h1>
+    <MainScreen page="bebidas" />
+  </div>
+);
+
+export default BebidasPage;
