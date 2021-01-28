78162789d6e94799b05806c84b1b24fb491d24b8
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2021-01-28 17:05:26 -0300

    Problemas de lint resolvidos

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,28 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Profile() {
-
   const eraseLocalStorage = () => {
     localStorage.clear();
-  }
+  };
 
   const profileElements = () => {
     const { email } = JSON.parse(localStorage.getItem('user'));
     return (
-      <div >
-        <h3 data-testid="profile-email" >{ email }</h3>
+      <div>
+        <h3 data-testid="profile-email">{ email }</h3>
         <Link to="/receitas-feitas">
-          <button data-testid="profile-done-btn" >Receitas Feitas</button>
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Receitas Feitas
+          </button>
         </Link>
         <Link to="/receitas-favoritas">
-          <button data-testid="profile-favorite-btn" >Receitas Favoritas</button>
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Receitas Favoritas
+          </button>
         </Link>
         <Link to="/">
-          <button onClick={() => eraseLocalStorage()} data-testid="profile-logout-btn" >Sair</button>
+          <button
+            type="button"
+            onClick={ () => eraseLocalStorage() }
+            data-testid="profile-logout-btn"
+          >
+            Sair
+          </button>
         </Link>
       </div>
-    )
-  }
+    );
+  };
 
   return (profileElements());
 }
