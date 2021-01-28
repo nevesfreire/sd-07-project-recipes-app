6711bdff70f680ace6b3a7f2b14d8d4580860f0b
alvaro <alvarovascon2@gmail.com>
2021-01-28 15:39:27 -0300

    atualizando branch

--- src/pages/ComidasID/index.js
+++ src/pages/ComidasID/index.js
@@ -2,28 +2,32 @@ import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchFoodById } from '../../redux/actions/foodActions';
+import { fetchCocktailByName } from '../../redux/actions/drinkActions';
 
 import shareIcon from '../../images/shareIcon.svg';
 import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
 
-function ComidasID({ meals, fetchFoodId }) {
+function ComidasID({ meals, fetchFoodId, fetchCocktails }) {
   const { id } = useParams();
 
   useEffect(() => {
     fetchFoodId(id);
+    fetchCocktails('');
   }, []);
+
+  if (!meals[0]) return (<p>Carregando...</p>);
 
   return (
     <div>
       <img
         data-testid="recipe-photo"
         alt=""
-        src=""
+        src={ meals[0].strMealThumb }
       />
       <h2
         data-testid="recipe-title"
       >
-        titulo
+        { meals[0].strMeal }
       </h2>
       <button type="button" data-testid="share-btn">
         <img alt="" src={ shareIcon } />
@@ -31,30 +35,50 @@ function ComidasID({ meals, fetchFoodId }) {
       <button type="button" data-testid="favorite-btn">
         <img alt="" src={ whiteHeartIcon } />
       </button>
-      <h4 data-testid="recipe-category">texto da categoria</h4>
-      {Object.keys(meals[0]).map((key) => {
-        if (meals[0][key] && key.includes('strIngredient')) {
-          return (<li>{meals[0][key]}</li>);
-        }
-        return null;
-      })}
-      <p data-testid="instructions">instruções</p>
-      {/** data-testid="video" para video */}
+      <h4 data-testid="recipe-category">{ meals[0].strCategory }</h4>
+      <ul>
+        Ingredientes:
+        {Object.keys(meals[0]).map((key, index) => {
+          const ingredientIndex = key.match(/(\d+)/);
+          if (meals[0][key] && key.includes('strIngredient')) {
+            const nine = 9;
+            const measure = `strMeasure${ingredientIndex[0]}`;
+            return (
+              <li
+                data-testid={ `${index - nine}-ingredient-name-and-measure` }
+              >
+                {`${meals[0][key]} - ${meals[0][measure]}`}
+              </li>
+            );
+          }
+          return null;
+        })}
+      </ul>
+      <p data-testid="instructions">{ meals[0].strInstructions }</p>
+      <a data-testid="video" href={ meals[0].strYoutube }>Assistir Vídeo</a>
       {/** card de recomendaçoes data-testid="${index}-recomendation-card" */}
-      <Link
-        to="/.../in-progress"
-        className="start-recipe-btn"
-        data-testid="start-recipe-btn"
-      >
-        iniciar/continuar receita
-        {/** esse botão desaparece caso receita já tenha sido feita */}
-      </Link>
+      <p>
+        <Link
+          to={ `/comidas/${id}/in-progress` }
+          className="start-recipe-btn"
+          data-testid="start-recipe-btn"
+        >
+          iniciar/continuar receita
+          {/** esse botão desaparece caso receita já tenha sido feita */}
+        </Link>
+      </p>
     </div>
   );
 }
 
-const mapStateToProps = (state) => ({ meals: state.foodMeals.meals });
+const mapStateToProps = (state) => ({
+  meals: state.foodMeals.meals,
+  drinks: state.cocktailsDrinks,
+});
 
-const mapDispatchToProps = { fetchFoodId: fetchFoodById };
+const mapDispatchToProps = {
+  fetchFoodId: fetchFoodById,
+  fetchCocktails: fetchCocktailByName,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ComidasID);
--- src/redux/reducers/drinkReducer.js
+++ src/redux/reducers/drinkReducer.js
@@ -2,10 +2,10 @@ const COCKTAIL_INITIAL_STATE = { cocktails: [], isFetching: false };
 
 function cocktailsDrinks(state = COCKTAIL_INITIAL_STATE, action) {
   switch (action.type) {
-  case 'REQUEST_FOOD':
+  case 'REQUEST_COCKTAIL':
     return { ...state, isFetching: true };
   case 'COCKTAIL':
-    return { cocktails: action.cocktails, isFetching: false };
+    return { cocktails: action.cocktails.drinks, isFetching: false };
   default:
     return state;
   }
