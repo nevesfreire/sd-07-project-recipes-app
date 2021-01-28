470d8588a80d3a4927d36d4a42be7252eb5ef549
Luiz Mariz <lfsmariz@gmail.com>
2021-01-28 17:07:55 -0300

    set new reducers for data elements

--- src/Context/reducer.js
+++ src/Context/reducer.js
@@ -3,6 +3,19 @@ export default function reducer(state, action) {
   case 'SET_USER':
     localStorage.setItem('user', JSON.stringify({ email: action.userEmail }));
     return { ...state, user: { userEmail: action.userEmail } };
+
+  case 'SET_MEALS':
+    return { ...state, mealsData: action.data };
+
+  case 'SET_COCKTAILS':
+    return { ...state, cocktailsData: action.data };
+
+  case 'SET_CATEGORIES_MEALS':
+    return { ...state, mealsCategory: action.data };
+
+  case 'SET_CATEGORIES_COCKTAILS':
+    return { ...state, cocktailsCategory: action.data };
+
   default:
     throw new Error();
   }
