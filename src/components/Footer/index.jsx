4291b1bb43d56d87a0a34bda945fe8ccaebfac40
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-01-28 20:06:57 -0300

    footer tests passing

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './style.css';
+
 import drinkIcon from '../../images/drinkIcon.svg';
 import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
 
 const Footer = () => (
-  <footer data-testid="footer">
-    <Link to="/bebidas" data-testid="drinks-bottom-btn">
-      <img src={ drinkIcon } alt="drink icon" />
+  <footer className="footer-bar" data-testid="footer">
+    <Link to="/bebidas">
+      <img src={ drinkIcon } alt="drink icon" data-testid="drinks-bottom-btn" />
     </Link>
-    <Link to="/explorar" data-testid="explore-bottom-btn">
-      <img src={ exploreIcon } alt="drink icon" />
+    <Link to="/explorar">
+      <img src={ exploreIcon } alt="drink icon" data-testid="explore-bottom-btn" />
     </Link>
-    <Link to="/comidas" data-testid="food-bottom-btn">
-      <img src={ mealIcon } alt="drink icon" />
+    <Link to="/comidas">
+      <img src={ mealIcon } alt="drink icon" data-testid="food-bottom-btn" />
     </Link>
   </footer>
 );
