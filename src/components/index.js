00dd74d793aa472a4aea8dd9384b0bfafb2d6818
Quintiliano Dalete <quintilianodalete@gmail.com>
2021-01-28 16:30:19 -0300

    feat: Implemented 9 to 12

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export { default as Footer } from './Footer';
 export { default as HeaderS } from './HeaderS';
 export { default as Header } from './Header';
+export { default as CardC } from './Card';
+export { default as CardList } from './CardList';
 // Cada componente tera uma exportacao conforme exemplos acima.
