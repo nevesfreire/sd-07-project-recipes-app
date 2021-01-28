5cd4539a9e60806321f06541db6dc77aa5137e40
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-01-28 18:16:17 -0300

    Terminando requisito 69

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -4,19 +4,37 @@ import Header from '../components/Header';
 import FlexContainer from '../components/FlexContainer';
 import ButtonLarge from '../components/ButtonLarge';
 import allActions from '../actions';
+import { useHistory } from 'react-router-dom';
 
-function Explore() {
+function Explore(props) {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(allActions.changePageTitle('Explorar'));
   }, [dispatch]);
 
+  const history = useHistory();
+
+  function handleClick(e, url) {
+    e.preventDefault();
+    history.push(`/explorar/${url}`);
+  }
+
   return (
     <FlexContainer>
       <Header />
-      <ButtonLarge data-testid="explore-food">Explorar Comidas</ButtonLarge>
-      <ButtonLarge data-testid="explore-drinks">Explorar Bebidas</ButtonLarge>
+      <ButtonLarge
+        data-testid="explore-food"
+        onClick={ (e) => handleClick(e, 'comidas') }
+      >
+        Explorar Comidas
+      </ButtonLarge>
+      <ButtonLarge
+        data-testid="explore-drinks"
+        onClick={ (e) => handleClick(e, 'bebidas') }
+      >
+        Explorar Bebidas
+      </ButtonLarge>
     </FlexContainer>
   );
 }
