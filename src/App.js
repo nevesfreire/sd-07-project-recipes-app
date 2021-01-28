c0e1d1ba6ad5b1620203cb07ecdc96a41b8f070c
berchon <berchon@gmail.com>
2021-01-28 03:55:25 -0300

    Feito o componente Footer - requisito 19 ao 24 - falta os testes - IMPEDIMENTOS: Requisito 21

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Provider from './context/Provider';
+import { Footer } from './components';
 
 function App() {
   return (
     <Provider>
       <BrowserRouter>
         <Switch>
-          {/* <Route exact path="/" component={ Login } /> */}
+          <Route exact path="/comidas" component={ Footer } />
           {/* <Redirect to="/404" /> */}
         </Switch>
       </BrowserRouter>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as BottomMenu } from './BottomMenu';
 export { default as Header } from './Header';
 export { default as SearchBar } from './SearchBar';
+export { default as Footer } from './Footer';
