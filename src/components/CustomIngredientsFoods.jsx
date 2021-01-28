67d1ef57dad1efaf10ffb4356a12d3cfe125fcee
marcelo ivan <sagacello@hotmail.com>
2021-01-28 19:08:56 -0300

    componente food ingredientes

---
+++ src/components/CustomIngredientsFoods.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+export default function CustomCardIngredientsFood({ index, meal }) {
+  const { strMeal, strMealThumb, idMeal } = meal;
+  return (
+    <div>
+      <Link to={ `/explorar/comidas/ingredientes/${idMeal}` }>
+        <p data-testid={ `${index}-ingredient-card` }>
+          { `receita ${index}` }
+        </p>
+        <img data-testid={ `${index}-card-img` } src={ strMealThumb } alt="" />
+        <p data-testid={ `${index}-card-name` }>{ strMeal }</p>
+      </Link>
+    </div>
+  );
+}
+
+CustomCardIngredientsFood.propTypes = {
+  index: PropTypes.number.isRequired,
+  meal: PropTypes.shape({
+    strMeal: PropTypes.string.isRequired,
+    strMealThumb: PropTypes.string.isRequired,
+    idMeal: PropTypes.number.isRequired,
+  }).isRequired,
+};
