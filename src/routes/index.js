4a5e76b4d747acf53db430ed8c80000de5e6c5ea
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-01-28 18:15:13 -0300

    Eslint eu te amo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-// eslint-disable-next-line max-len
-import { TelaDeLogin, TelaPrincipalReceitasBebidas, TelaPrincipalReceitasComidas } from '../pages';
+import {
+  TelaPrincipalReceitasBebidas,
+  TelaPrincipalReceitasComidas,
+  Explorar,
+  ExplorarComidas,
+  ExplorarBebidas,
+  ExpComidasIng,
+  ExpBebidasIng,
+  ExplorarComidasLoc,
+  Perfil,
+  ReceitasFeitas,
+  ReceitasFavoritas,
+} from '../pages';
 
 const Routes = () => (
   <Switch>
-    <Route exact path="/" component={ TelaDeLogin } />
-    <Route path="/bebida" component={ TelaPrincipalReceitasBebidas } />
-    <Route path="/comida" component={ TelaPrincipalReceitasComidas } />
+    <Route exact path="/bebidas" component={ TelaPrincipalReceitasBebidas } />
+    <Route exact path="/comidas" component={ TelaPrincipalReceitasComidas } />
+    <Route exact path="/explorar" component={ Explorar } />
+    <Route exact path="/explorar/comidas" component={ ExplorarComidas } />
+    <Route exact path="/explorar/bebidas" component={ ExplorarBebidas } />
+    <Route exact path="/explorar/comidas/ingredientes" component={ ExpComidasIng } />
+    <Route exact path="/explorar/bebidas/ingredientes" component={ ExpBebidasIng } />
+    <Route exact path="/explorar/comidas/area" component={ ExplorarComidasLoc } />
+    <Route exact path="/perfil" component={ Perfil } />
+    <Route exact path="/receitas-feitas" component={ ReceitasFeitas } />
+    <Route exact path="/receitas-favoritas" component={ ReceitasFavoritas } />
   </Switch>
 );
 
