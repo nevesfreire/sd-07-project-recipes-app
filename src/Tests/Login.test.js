9aae87e355fa5b9dc5a889e0d921cc622a740733
Fabio Camargos <fabiocmmcm@gmail.com>
2021-01-28 15:04:28 -0300

    testeLogin

--- src/Tests/Login.test.js
+++ src/Tests/Login.test.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
+import {cleanup} from '@testing-library/react'
 import renderWithRouter from './renderWithRouter';
 import Login from '../pages/Login';
 import App from '../App';
 
 describe('Teste página de Login', () => {
-  it('Crie os elementos que devem respeitar os atributos descritos no protótipo', () => {
-    const { getByTestId } = renderWithRouter(<Login />);
-    const emailTest = getByTestId('email-input');
-    const passwordTest = getByTestId('password-input');
-    const buttonLoginTest = getByTestId('login-submit-btn');
+  const { getByTestId, history } = renderWithRouter(<Login />);
+  const email = getByTestId('email-input');
+  const senha = getByTestId('password-input');
+  const button = getByTestId('login-submit-btn');
+  
+  afterEach(cleanup)
 
-    expect(emailTest).toBeInTheDocument();
-    expect(passwordTest).toBeInTheDocument();
-    expect(buttonLoginTest).toBeInTheDocument();
+  it('Crie os elementos que devem respeitar os atributos descritos no protótipo', () => {
+    expect(email).toBeInTheDocument();
+    expect(senha).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
   });
 
   it('o formulário abilitado só após a validação de email e senha', () => {
-    const { getByTestId } = renderWithRouter(<Login />);
-    const button = getByTestId('login-submit-btn');
     expect(button).toBeDisabled();
-    const email = getByTestId('email-input');
-    const senha = getByTestId('password-input');
     userEvent.type(email, 'email');
     userEvent.type(senha, '123456');
     expect(button).toBeDisabled();
@@ -32,17 +31,13 @@ describe('Teste página de Login', () => {
     userEvent.type(senha, '123456');
     expect(button).toBeDisabled();
     userEvent.type(email, 'alguem@email.com');
-    userEvent.type(senha, '1234567');
+    userEvent.type(senha, '123456');
     expect(button).toBeEnabled();
   });
 
   it(' Redirecione para a tela principal de receitas após a submissão do login', () => {
-    const { getByTestId, history } = renderWithRouter(<App />);
-    const email = getByTestId('email-input');
-    const senha = getByTestId('password-input');
-    const button = getByTestId('login-submit-btn');
     userEvent.type(email, 'alguem@email.com');
-    userEvent.type(senha, '1234567');
+    userEvent.type(senha, '123456');
     userEvent.click(button);
     const { pathname } = history.location;
     expect(pathname).toBe('/comidas');
