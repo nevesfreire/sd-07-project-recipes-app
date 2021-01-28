901769328b32dafed4630007618a3b2747be965e
Luiz Mariz <lfsmariz@gmail.com>
2021-01-28 17:06:43 -0300

    update initState

--- src/Context/initialState.js
+++ src/Context/initialState.js
@@ -2,6 +2,10 @@ const initialState = {
   user: {
     userEmail: '',
   },
+  mealsData: [],
+  cocktailsData: [],
+  mealsCategory: [],
+  cocktailsCategory: [],
 };
 
 export default initialState;
