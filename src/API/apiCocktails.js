196445b0f7de0d9b72bc1eb0b05ae2192c80b91a
Luiz Mariz <lfsmariz@gmail.com>
2021-01-28 18:53:58 -0300

    generalize fetch

--- src/API/apiCocktails.js
+++ src/API/apiCocktails.js
@@ -1,8 +1,16 @@
 const start = 0;
 const end = 12;
 
-export const fetchAllCocktails = async (searcher = '', initArr = start, endArr = end) => {
-  const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searcher}`;
+export const fetchAllCocktails = async (
+  searcher = '',
+  category = '',
+  initArr = start,
+  endArr = end,
+) => {
+  const URL_ALL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searcher}`;
+  const URL_CATEGORY = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`;
+
+  const URL = category === '' ? URL_ALL : URL_CATEGORY;
   const resolve = await fetch(URL);
 
   const resolveJson = await resolve.json();
--- src/API/apiMeals.js
+++ src/API/apiMeals.js
@@ -1,8 +1,16 @@
 const start = 0;
 const end = 12;
 
-export const fetchAllRecipes = async (searcher = '', initArr = start, endArr = end) => {
-  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searcher}`;
+export const fetchAllRecipes = async (
+  searcher = '',
+  category = '',
+  initArr = start,
+  endArr = end,
+) => {
+  const URL_ALL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searcher}`;
+  const URL_CATEGORY = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+
+  const URL = category === '' ? URL_ALL : URL_CATEGORY;
   const resolve = await fetch(URL);
 
   const resolveJson = await resolve.json();
