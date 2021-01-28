6a9e2d5e95e32355f36b5de9329150abc1e52378
Carol Bezerra <carol.arbe@gmail.com>
2021-01-28 11:50:36 -0300

    intro redux

---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import thunk from 'redux-thunk';
+
+// // const rootReducer = combineReducers({});
+
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//   ),
+// );
+
+// const composeEnhancers = window
+//   .__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//     trace: true,
+//     traceLimit: 25,
+//   })
+//   || compose;
+// const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+// export default store;
