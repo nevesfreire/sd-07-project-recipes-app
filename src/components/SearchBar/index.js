b841c87c818e936604e35d18fda81f92c1c34d90
Carlos Souza <carlos.pessoal@hotmail.com>
2021-01-28 15:26:50 -0300

    Comitão requisitos 11 e 12

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <div>
+      <input
+        type="text"
+        data-testid="search-input"
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
