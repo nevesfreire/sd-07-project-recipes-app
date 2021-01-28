7f577f55ad89cd3603434edd827b18b49bed33bd
MoisesSantana <moisaant@gmail.com>
2021-01-28 19:10:00 -0300

    testa se o botão desabilita e habilita

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import Login from '../components/Login'
 
@@ -18,5 +18,13 @@ describe('Component Login', () => {
 
     const textEnterButton = screen.getByText(/enter/i);
     expect(textEnterButton).toBeInTheDocument();
+
+    const disabledEnterButton = screen.getByRole('button');
+    expect(disabledEnterButton).toHaveAttribute('disabled');
+
+    fireEvent.change(emailInput, {target: {value: 'example@email.com'}});
+    fireEvent.change(passwordInput, {target: {value: '123456'}});
+
+    expect(disabledEnterButton).not.toHaveAttribute('disabled');
   });
 });
