e2b4f45848bba361a07bab319b06f9a568a104f1
Felipe Nascimento <flpnascto@gmail.com>
2021-01-28 15:49:21 -0300

    Menu inferior

--- src/pages/Foods/index.js
+++ src/pages/Foods/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Footer from '../Footer';
 
 export default function Foods() {
   return (
     <div>
       <Header title="Comidas" />
+      <Footer />
     </div>
   );
 }
