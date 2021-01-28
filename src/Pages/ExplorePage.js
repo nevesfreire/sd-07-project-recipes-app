6acf95f76def6a261edbfce163d7021de1f672c8
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-01-28 16:13:40 -0300

    feat: explore page.

--- src/Pages/ExplorePage.js
+++ src/Pages/ExplorePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 
@@ -6,7 +7,24 @@ function ExplorePage() {
   return (
     <div>
       <Header title="Explorar" />
-      Explore
+      <main>
+        <Link to="/explorar/comidas">
+          <button
+            type="button"
+            data-testid="explore-food"
+          >
+            Explorar Comidas
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button
+            type="button"
+            data-testid="explore-drinks"
+          >
+            Explorar Bebidas
+          </button>
+        </Link>
+      </main>
       <Footer />
     </div>
   );
