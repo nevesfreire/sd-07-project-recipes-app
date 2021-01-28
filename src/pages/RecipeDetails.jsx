56513e77d703780796731ea14c11d3bb663e8b6c
danielmlduarte <danielduarte.tryber@gmail.com>
2021-01-28 15:20:47 -0300

    Requisito 35

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -7,6 +7,7 @@ export default class RecipeDetails extends Component {
     this.fetchRecipe = this.fetchRecipe.bind(this);
     this.getTypeOfRecipe = this.getTypeOfRecipe.bind(this);
     this.getYoutubeEmbedUrl = this.getYoutubeEmbedUrl.bind(this);
+    this.getIngredientsList = this.getIngredientsList.bind(this);
     this.state = {
       isLoading: true,
       recipe: {},
@@ -29,7 +30,33 @@ export default class RecipeDetails extends Component {
     const { recipe: { strYoutube } } = this.state;
     const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
     const match = strYoutube.match(regExp);
+    this.getIngredientsList();
     return `https://www.youtube.com/embed/${match[2]}`;
+  }
+
+  getIngredientsList() {
+    const { recipeType, recipe } = this.state;
+    const INITIAL_INDEX = 1;
+    const INDEX_FOOD = 20;
+    const INDEX_DRINK = 15;
+    const MAX_INDEX = (recipeType === 'comidas') ? INDEX_FOOD : INDEX_DRINK;
+    const ingredientsAndMeasureList = [];
+    for (let index = INITIAL_INDEX; index < MAX_INDEX; index += 1) {
+      ingredientsAndMeasureList
+        .push([recipe[`strIngredient${index}`], recipe[`strMeasure${index}`]]);
+    }
+    return (
+      ingredientsAndMeasureList
+        .filter((ingredient) => ingredient[0] !== '' && ingredient[0] !== null)
+        .map((ingredient, index) => (
+          <li
+            key={ index }
+            data-testid={ `${index}-ingredient-name-and-measure` }
+          >
+            { `${(ingredient[1]) ? (ingredient[1]) : ''} ${ingredient[0]}` }
+          </li>
+        ))
+    );
   }
 
   async fetchRecipe(id, path) {
@@ -77,9 +104,9 @@ export default class RecipeDetails extends Component {
           { (recipeType === 'comidas') ? recipe.strCategory : recipe.strAlcoholic }
         </h3>
         <p data-testid="instructions">{ strInstructions }</p>
-        <li>
-          <ul data-testid="${index}-ingredient-name-and-measure"> INGREDIENT</ul>
-        </li>
+        <ul>
+          { (!isLoading) && this.getIngredientsList() }
+        </ul>
         { (recipeType === 'comidas') && <iframe
           data-testid="video"
           src={ this.getYoutubeEmbedUrl() }
