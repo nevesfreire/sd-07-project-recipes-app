d01aa96fafa21764decf9798d8b6442dfe274958
MoisesSantana <moisaant@gmail.com>
2021-01-29 17:55:49 -0300

    componente HeaderSearch estruturado

--- src/components/HeaderSearch.jsx
+++ src/components/HeaderSearch.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
 
 function HeaderSearch() {
-  return <div>oi</div>;
+  return (
+    <section>
+      <div>
+        <input
+          type="text"
+          name="searchInput"
+          id="searchInput"
+          placeholder="Buscar Receita"
+          data-testid="search-input"
+        />
+      </div>
+      <div>
+        <label htmlFor="ingredientSearch">
+          <input
+            type="radio"
+            name="searchFilter"
+            id="ingredientSearch"
+            data-testid="ingredient-search-radio"
+          />
+          Ingrediente
+        </label>
+        <label htmlFor="nameSearch">
+          <input
+            type="radio"
+            name="searchFilter"
+            id="nameSearch"
+            data-testid="name-search-radio"
+          />
+          Nome
+        </label>
+        <label htmlFor="firstLetterSearch">
+          <input
+            type="radio"
+            name="searchFilter"
+            id="firstLetterSearch"
+            data-testid="first-letter-search-radio"
+          />
+          Primeira letra
+        </label>
+      </div>
+
+      <div>
+        <button
+          type="button"
+          data-testid="exec-search-btn"
+        >
+          Buscar
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default HeaderSearch;
