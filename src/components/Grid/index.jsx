9372f09d5de02f139b2346b882a3b85c238d1e25
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-01-28 22:18:04 -0300

    fixed errors with explore pages

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -1,67 +1,170 @@
 import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import './style.css';
 
 import Context from '../../Context';
 import Meals from '../../services/meals-api';
+import Drinks from '../../services/cocktails-api';
 
-const Grid = () => {
+const Grid = ({ page }) => {
   const {
-    data,
-    setData,
-    categoryList,
-    setCategoryList,
-    filteredData,
-    handleClickCategory,
-  } = useContext(Context);
+    mealsData,
+    setMealsData,
+    mealsCategoryList,
+    setMealsCategoryList,
+    mealsFilteredData,
+    handleClickCategoryMeals,
+  } = useContext(Context).meal;
+  const {
+    drinksData,
+    setDrinksData,
+    drinksCategoryList,
+    setDrinksCategoryList,
+    drinksFilteredData,
+    handleClickCategoryDrinks,
+  } = useContext(Context).drink;
 
   useEffect(() => {
     const mealsAmountToShow = 12;
 
-    Meals.getMeals(mealsAmountToShow)
-      .then((res) => setData(res))
-      .catch((err) => console.log(err));
-  }, [setData]);
+    if (page === 'meals') {
+      Meals.getMeals(mealsAmountToShow)
+        .then((res) => setMealsData(res))
+        .catch((err) => console.log(err));
+    }
+
+    if (page === 'drinks') {
+      Drinks.getCocktails(mealsAmountToShow)
+        .then((res) => setDrinksData(res))
+        .catch((err) => console.log(err));
+    }
+  }, [page, setMealsData, setDrinksData]);
 
   useEffect(() => {
     const categoriesAmountToShow = 5;
 
-    Meals.getMealCategoryList(categoriesAmountToShow)
-      .then((res) => setCategoryList(res))
-      .catch((err) => console.log(err));
-  }, [setCategoryList]);
+    if (page === 'meals') {
+      Meals.getMealCategoryList(categoriesAmountToShow)
+        .then((res) => setMealsCategoryList(res))
+        .catch((err) => console.log(err));
+    }
 
-  if (!data.length || !categoryList.length) {
+    if (page === 'drinks') {
+      Drinks.getCocktailCategoryList(categoriesAmountToShow)
+        .then((res) => setDrinksCategoryList(res))
+        .catch((err) => console.log(err));
+    }
+  }, [page, setMealsCategoryList, setDrinksCategoryList]);
+
+  const loadingData = () => {
+    if (page === 'meals') {
+      return mealsData.length || mealsCategoryList.length;
+    }
+
+    return drinksData.length || drinksCategoryList.length;
+  };
+
+  if (!loadingData()) {
     return <h1>Loading meals...</h1>;
+  }
+
+  if (page === 'meals') {
+    return (
+      <>
+        <div>
+          <Link to="/bebidas">Bebidas</Link>
+
+          <button
+            type="button"
+            data-testid="All-category-filter"
+            onClick={ () => handleClickCategoryMeals('All') }
+          >
+            All
+          </button>
+          {
+            mealsCategoryList.map(({ strCategory }) => (
+              <button
+                type="button"
+                key={ strCategory }
+                data-testid={ `${strCategory}-category-filter` }
+                onClick={ () => handleClickCategoryMeals(strCategory) }
+              >
+                { strCategory}
+              </button>
+            ))
+          }
+        </div>
+        <div className="grid-list">
+          {
+            mealsFilteredData.map(({ idMeal, strMeal, strMealThumb }, index) => (
+              <Link
+                to={ `/comidas/${idMeal}` }
+                key={ idMeal }
+                data-testid={ `${index}-recipe-card` }
+              >
+                <img
+                  className="recipe-img"
+                  width="150"
+                  src={ strMealThumb }
+                  alt="meal"
+                  data-testid={ `${index}-card-img` }
+                />
+                <h2 data-testid={ `${index}-card-name` }>
+                  {strMeal}
+                </h2>
+              </Link>
+            ))
+          }
+        </div>
+      </>
+    );
   }
 
   return (
     <>
       <div>
-        <button type="button" onClick={ () => handleClickCategory('All') }>
+        <Link to="/comidas">Comidas</Link>
+
+        <button
+          type="button"
+          data-testid="All-category-filter"
+          onClick={ () => handleClickCategoryDrinks('All') }
+        >
           All
         </button>
         {
-          categoryList.map(({ strCategory }) => (
+          drinksCategoryList.map(({ strCategory }) => (
             <button
               type="button"
               key={ strCategory }
               data-testid={ `${strCategory}-category-filter` }
-              onClick={ () => handleClickCategory(strCategory) }
+              onClick={ () => handleClickCategoryDrinks(strCategory) }
             >
               { strCategory}
             </button>
           ))
         }
       </div>
-      <div>
+      <div className="grid-list">
         {
-          filteredData.map(({ idMeal, strMeal, strMealThumb }) => (
+          drinksFilteredData.map(({ idDrink, strDrink, strDrinkThumb }, index) => (
             <Link
-              to={ `/comidas/${idMeal}` }
-              key={ idMeal }
+              to={ `/bebidas/${idDrink}` }
+              key={ idDrink }
+              data-testid={ `${index}-recipe-card` }
             >
-              <img src={ strMealThumb } alt="meal" />
-              <h2>{strMeal}</h2>
+              <img
+                className="recipe-img"
+                width="150"
+                src={ strDrinkThumb }
+                alt="drink"
+                data-testid={ `${index}-card-img` }
+              />
+              <h2 data-testid={ `${index}-card-name` }>
+                {strDrink}
+              </h2>
             </Link>
           ))
         }
@@ -69,5 +172,7 @@ const Grid = () => {
     </>
   );
 };
+
+Grid.propTypes = { page: PropTypes.string.isRequired };
 
 export default Grid;
