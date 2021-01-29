f05d83bb7963f7e593aa9d1571203beefd3bea05
Lucas <lafdesouza2002@outlook.com>
2021-01-28 21:00:35 -0300

    Começando tela principal

--- src/pages/Bebidas.js
+++ src/pages/Bebidas.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SearchButton from '../components/SearchButton';
 
 export default function Bebidas() {
+  const [cards, setCards] = useState([]);
+
+  const getCards = async () => {
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    const data = await response.json();
+    const initialIndex = 0;
+    const finalIndex = 12;
+    setCards(data.drinks.splice(initialIndex, finalIndex));
+  };
+
+  useEffect(() => {
+    getCards();
+  }, []);
+
   return (
     <div>
       <Header />
       <SearchButton />
+      {cards.map((card, index) => (
+        <div key={ card.id } data-testid={ `${index}-recipe-card` }>
+          <img
+            data-testid={ `${index}-card-img` }
+            src={ card.strDrinkThumb }
+            alt="Thumb Bebida"
+          />
+          <h1 data-testid={ `${index}-card-name` }>{card.strDrink}</h1>
+        </div>
+      ))}
       <Footer />
     </div>
   );
--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import SearchButton from '../components/SearchButton';
 
 export default function Comidas() {
+  const [cards, setCards] = useState([]);
+
+  const getCards = async () => {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    const data = await response.json();
+    console.log(data.meals);
+    const initialIndex = 0;
+    const finalIndex = 12;
+    setCards(data.meals.splice(initialIndex, finalIndex));
+  };
+
+  useEffect(() => {
+    getCards();
+  }, []);
+
   return (
     <div>
       <Header />
       <SearchButton />
       <SearchBar />
+      {cards.map((card, index) => (
+        <div key={ card.id } data-testid={ `${index}-recipe-card` }>
+          <img
+            data-testid={ `${index}-card-img` }
+            src={ card.strMealThumb }
+            alt="Thumb Comida"
+          />
+          <h1 data-testid={ `${index}-card-name` }>{card.strMeal}</h1>
+        </div>
+      ))}
       <Footer />
     </div>
   );
