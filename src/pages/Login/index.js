0e7f52e98e87074b1676c6cce015167109a0b200
Elisa <elisatcamillo@gmail.com>
2021-01-28 23:04:49 -0300

    changed email+password validation method

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import signIn from '../../store/ducks/auth/actions';
 
-const Login = () => <h1>PAGE LOGIN</h1>;
+const Login = () => {
+  const dispatch = useDispatch();
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const isDisabled = () => {
+    const { email, password } = user;
+    const regexp = /\S+@\S+\.\S+/;
+    const six = 6;
+    if (regexp.test(email) && password.length > six) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    dispatch(signIn(user.email));
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          data-testid="email-input"
+          name="email"
+          onChange={ handleChange }
+        />
+        <input
+          data-testid="password-input"
+          name="password"
+          onChange={ handleChange }
+        />
+        <Link to="/comidas">
+          <button
+            type="submit"
+            data-testid="login-submit-btn"
+            disabled={ isDisabled() }
+            onClick={ handleSubmit }
+          >
+            Enter
+          </button>
+        </Link>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Home, Login } from '../pages';
+import { Home, Login } from '../pages/index';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/receitas" component={ Home } />
+      <Route exact path="/bebidas" component={ Home } />
       <Route exact path="/comidas" component={ Home } />
       <Route exact path="/" component={ Login } />
     </Switch>
