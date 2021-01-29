2dc9531a330fb9799b23cb181c17a6eb6e717c85
Anderson Santana <anderslash@gmail.com>
2021-01-29 00:16:47 -0300

    Background de comida e ajuste de dimensoes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -43,7 +43,7 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="body-container">
+      <div className="background-image">
         <div className="input-group">
           <div className="title-container">
             <div>
