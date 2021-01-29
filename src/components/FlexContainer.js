1a3c9ce9271a72fab3dacc638f43a8d1302280f7
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-01-28 22:06:09 -0300

    Iniciando requisito 33

--- src/components/FlexContainer.js
+++ src/components/FlexContainer.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const FlexContainer = styled.div`
   display: flex;
   flex-flow: row wrap;
-  justify-content: space-around;
+  justify-content: space-between;
 `;
 
 export default FlexContainer;
