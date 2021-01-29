3b5d022dc21292a8c8f96fcfc047018dc702b8d3
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2021-01-29 17:59:57 -0300

    cards

--- src/components/card/CardComida.js
+++ src/components/card/CardComida.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class CardComidas extends React.Component {
   render() {
+    const { image, title } = this.props;
     return (
       <div>
-        CardComidas
+        <img src={ image } alt="recipe pic" />
+        <div>{title}</div>
       </div>
     );
   }
 }
 export default CardComidas;
+
+CardComidas.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
