87f8a408cea82faf44b3d9e2c5f9f70d536bba52
sttefanieMaciel <macielsttefanie@gmail.com>
2021-01-29 18:11:30 -0300

    Esqueleto da página BebidasIngredientes feito

---
+++ src/pages/bebidas/BebidasIngredientes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExplorarIngredientes from '../../components/ExplorarIngredientes';
+import Footer from '../../components/footer/Footer';
+
+function BebidasIngredientes() {
+  return (
+    <div>
+      <ExplorarIngredientes />
+      <Footer />
+    </div>
+  );
+}
+export default BebidasIngredientes;
