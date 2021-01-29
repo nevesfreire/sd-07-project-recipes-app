ff0fecb6d0c26abbaa54a8e0568f6b0966fef3f7
lisvalver <lisvalver.atte@gmail.com>
2021-01-29 18:06:28 -0300

    Tela de explorar bebidas ou comidas - Req 70 a 74

--- src/reducers/searchToggleReducer.js
+++ src/reducers/searchToggleReducer.js
@@ -1,9 +1,11 @@
-import type from '../actions';
+import {
+  SEARCH_TOGGLE,
+} from '../actions';
 
 const INITIAL_STATE = false;
 
 const searchToggleReducer = (state = INITIAL_STATE, action) => {
-  if (action.type === type.SEARCH_TOGGLE) {
+  if (action.type === SEARCH_TOGGLE) {
     return action.toggle;
   }
   return state;
