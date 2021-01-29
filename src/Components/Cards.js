c5347f9fa91132f118370a38402b4a4822593889
Pablo <pablopessanh@gmail>
2021-01-29 15:36:04 -0300

    Requisito 32

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function Cards({ card, context, index }) {
   const title = context === 'Comidas' ? card.strMeal : card.strDrink;
   const image = context === 'Comidas' ? card.strMealThumb : card.strDrinkThumb;
+  const id = context === 'Comidas' ? card.idMeal : card.idDrink;
+  const pageContext = context;
+
   return (
-    <div data-testid={ `${index}-recipe-card` }>
-      <h3 data-testid={ `${index}-card-name` }>{title}</h3>
-      <img
-        data-testid={ `${index}-card-img` }
-        src={ image }
-        alt="Imagem da comida ou bebida"
-        width="200"
-      />
-    </div>
+    <Link to={ `/${pageContext.toLowerCase()}/${id}` }>
+      <div data-testid={ `${index}-recipe-card` }>
+        <h3 data-testid={ `${index}-card-name` }>{title}</h3>
+        <img
+          data-testid={ `${index}-card-img` }
+          src={ image }
+          alt="Imagem da comida ou bebida"
+          width="200"
+        />
+      </div>
+    </Link>
   );
 }
 
@@ -23,6 +29,8 @@ Cards.propTypes = {
     strDrink: PropTypes.string,
     strMealThumb: PropTypes.string,
     strDrinkThumb: PropTypes.string,
+    idMeal: PropTypes.string,
+    idDrink: PropTypes.string,
   }).isRequired,
   context: PropTypes.string.isRequired,
   index: PropTypes.number.isRequired,
