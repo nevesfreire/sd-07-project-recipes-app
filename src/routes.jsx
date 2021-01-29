1c5e21557320f1b2d4f037fe0bc9e2d8206f7fa8
Luiz Mariz <lfsmariz@gmail.com>
2021-01-29 07:12:49 -0300

    route update

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import BebidasPage from './Pages/BebidasPage';
-import ComidaPage from './Pages/ComidaPage';
-=======
 import DrinkPage from './Pages/DrinkPage';
->>>>>>> main-group-8
 import LoginPage from './Pages/LoginPage';
 import HomePage from './Pages/HomePage';
 import ExplorarPage from './Pages/ExplorarPage';
@@ -26,25 +21,6 @@ const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={ LoginPage } />
-<<<<<<< HEAD
-      <Route exact path="/comidas" component={ ComidaPage } />
-      <Route exact path="/bebidas" component={ BebidasPage } />
-      {/* TODO PAGES */}
-      {/*
-      <Route exact path="/comidas/:id-receita" component={  } />
-      <Route exact path="/bebidas/:id-receita" component={  } />
-      <Route exact path="/comidas/:id-receita/in-progress" component={  } />
-      <Route exact path="/bebidas/:id-receita/in-progress" component={  } />
-      <Route exact path="/explorar" component={  } />
-      <Route exact path="/explorar/comidas" component={  } />
-      <Route exact path="/explorar/bebidas" component={  } />
-      <Route exact path="/explorar/comidas/ingredientes" component={  } />
-      <Route exact path="/explorar/bebidas/ingredientes" component={  } />
-      <Route exact path=" /explorar/comidas/area" component={  } />
-      <Route exact path="/perfil" component={  } />
-      <Route exact path="/receitas-feitas" component={  } />
-      <Route exact path="/receitas-favoritas" component={  } /> */}
-=======
       <Route exact path="/comidas" component={ HomePage } />
       <Route exact path="/bebidas" component={ DrinkPage } />
       <Route exact path="/explorar" component={ ExplorarPage } />
@@ -76,7 +52,6 @@ const Routes = () => (
       <Route exact path="/explorar/comidas/area" component={ ComidaArea } />
       <Route exact path="/receitas-feitas" component={ ReceitaFeitas } />
       <Route exact path="/receitas-favoritas" component={ ReceitaFavorita } />
->>>>>>> main-group-8
     </Switch>
   </BrowserRouter>
 );
