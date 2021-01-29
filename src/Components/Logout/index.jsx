6cebd36052f3c7781028461cdeaf39d93217cc61
Filipi Firmino <contatoic3web@gmail.com>
2021-01-29 18:01:07 -0300

    Profile user redirect for / and clear storage local user key

--- src/Components/Logout/index.jsx
+++ src/Components/Logout/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const LogoutButton = () => {
-  const logout = () => console.log('saiu');
+  const history = useHistory();
+  const logout = () => {
+    localStorage.clear('user');
+    history.push('/');
+  };
 
   return (
     <button
       type="button"
       className="btn-logout"
       data-testid="profile-logout-btn"
-      onClick={ logout() }
+      onClick={ () => logout() }
     >
       Sair
     </button>
