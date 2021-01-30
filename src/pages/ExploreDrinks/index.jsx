4940ddf407e8d6a3e348034d2b365da9d144ef98
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-01-30 19:21:07 -0300

    Requisitos 70, 71, 72 e 73

--- src/pages/ExploreDrinks/index.jsx
+++ src/pages/ExploreDrinks/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Header, Footer } from '../../components';
 
 export default function ExploreDrinks() {
   return (
     <div>
       <Header title="Explorar Bebidas" />
+      <div>
+        <Link
+          data-testid="explore-by-ingredient"
+          to="/explorar/bebidas/ingredientes"
+        >
+          Por Ingredientes
+        </Link>
+        <Link
+          data-testid="explore-surprise"
+          to="/explorar/bebidas"
+        >
+          Me Surpreenda!
+        </Link>
+
+      </div>
       <Footer />
     </div>
   );
