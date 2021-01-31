dc9c409845acd6077b636f0b42693b0d03ee8834
MoisesSantana <moisaant@gmail.com>
2021-01-31 17:15:03 -0300

    componente footer OK

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import '../styles/components/footer.css';
 
 function Footer() {
   return (
-    <footer data-testid="footer">
-      <button
-        type="button"
-        data-testid="drinks-bottom-btn"
-      >
-        Bebidas
-      </button>
-      <button
-        type="button"
-        data-testid="explore-bottom-btn"
-      >
-        Explorar
-      </button>
-      <button
-        type="button"
-        data-testid="food-bottom-btn"
-      >
-        Comidas
-      </button>
+    <footer className="footer" data-testid="footer">
+      <Link to="/bebidas">
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="drinks"
+        />
+      </Link>
+      <Link to="/explorar">
+        <img
+          data-testid="explore-bottom-btn"
+          src={ exploreIcon }
+          alt="explore"
+        />
+      </Link>
+      <Link to="/comidas">
+        <img
+          data-testid="food-bottom-btn"
+          src={ mealIcon }
+          alt="meal"
+        />
+      </Link>
     </footer>
   );
 }
