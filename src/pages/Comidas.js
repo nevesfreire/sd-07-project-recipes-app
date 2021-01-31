c9ad31c95d5bb51417db325fa136fee55f2d6708
CarolSi <a.carolg1@gmail.com>
2021-01-31 13:45:17 -0300

    main-group-13-requisito28-aproveitando estabilidade da API lint

--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import CardList from '../components/CardList';
@@ -25,8 +24,6 @@ function Comidas() {
   const noFindRecipe = 'Sinto muito, não encontramos nenhuma receita para esses filtros.';
 
   const magicNumberZero = 0;
-
-  const history = useHistory();
 
   const getGlobalMealData = async () => {
     const data = await fetchGlobalMeal();
