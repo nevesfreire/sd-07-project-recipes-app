cdbfdcbd27e02bc8208eace2dd87de32f5f6c313
moacyrrln <moacyrrln@gmail.com>
2021-01-31 12:10:50 -0300

    main-group-11-requisitos-13-14-15-16-ok

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,24 +1,98 @@
 import React, { useContext } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import RecipesContext from '../../context/RecipesContext';
+import RequestData from '../../services/RequestAPI';
 
 const SearchBar = () => {
   const {
     recipesInput,
-    handleRadioChange,
+    recipesRadio,
     handleRecipesInput,
-    handleClickFood,
-    handleClickDrink } = useContext(RecipesContext);
+    handleRadioChange } = useContext(RecipesContext);
 
-  const LocationDisplay = useLocation().pathname;
+  const { location, push } = useHistory();
+  console.log(location.pathname);
+
+  let URL = '';
+  let xablau;
+
+  const handleClickFood = async () => {
+    await console.log('Food');
+    switch (recipesRadio) {
+    case 'Ingrediente':
+      URL = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${recipesInput}`;
+      await fetch(URL)
+        .then((r) => r.json())
+        .then((r) => {
+          if (r.meals.length !== 1) {
+            console.log(r);
+            console.log(r.meals.length);
+          } else console.log('1');
+        });
+      break;
+    case 'Nome':
+      URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${recipesInput}`;
+      xablau = await RequestData(URL);
+      if (xablau.meals.length === 1) {
+        push(`/comidas/${xablau.meals[0].idMeal}`);
+      }
+      break;
+    case 'Primeira letra':
+      if (recipesInput.length === 1) {
+        URL = `https://www.themealdb.com/api/json/v1/1/search.php?f=${recipesInput}`;
+        await fetch(URL)
+          .then((r) => r.json())
+          .then((r) => console.log(r));
+      } else {
+        alert('Sua busca deve conter somente 1 (um) caracter');
+      }
+      break;
+    default:
+      break;
+    }
+  };
+
+  const handleClickDrink = async () => {
+    console.log('Drink');
+    switch (recipesRadio) {
+    case 'Ingrediente':
+      URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${recipesInput}`;
+      fetch(URL)
+        .then((r) => r.json())
+        .then((r) => console.log(r));
+      break;
+    case 'Nome':
+      URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${recipesInput}`;
+      xablau = await RequestData(URL);
+      console.log(xablau.drinks[0].idDrink);
+      console.log(xablau.drinks)
+      if (xablau.drinks.length === 1) {
+        push(`/bebidas/${xablau.drinks[0].idDrink}`);
+      }
+      break;
+    case 'Primeira letra':
+      if (recipesInput.length === 1) {
+        URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${recipesInput}`;
+        fetch(URL)
+          .then((r) => r.json())
+          .then((r) => console.log(r));
+      } else {
+        alert('Sua busca deve conter somente 1 (um) caracter');
+      }
+      break;
+    default:
+      break;
+    }
+  };
 
   let funcFood = false;
   let funcDrink = false;
 
-  if (LocationDisplay === '/comidas') {
+  if (location.pathname === '/comidas') {
     funcFood = true;
   }
-  if (LocationDisplay === '/bebidas') {
+
+  if (location.pathname === '/bebidas') {
     funcDrink = true;
   }
 
@@ -58,6 +132,7 @@ const SearchBar = () => {
       <button
         type="button"
         data-testid="exec-search-btn"
+        // onClick={ handleClickFood }
         onClick={ funcFood && handleClickFood || funcDrink && handleClickDrink }
       >
         Buscar
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
 import RecipesContext from './RecipesContext';
+import RequestData from '../services/RequestAPI';
 
 const RecipesProvider = ({ children }) => {
   const [recipesInput, setRecipesInput] = useState('');
   const [recipesRadio, setRecipesRatio] = useState('');
-  const [data, setData] = useState();
 
   const handleRecipesInput = (event) => {
     setRecipesInput(event.target.value);
@@ -15,75 +16,11 @@ const RecipesProvider = ({ children }) => {
     setRecipesRatio(event.target.value);
   };
 
-  let URL = '';
-
-  const handleClickFood = () => {
-    console.log('Food');
-    switch (recipesRadio) {
-    case 'Ingrediente':
-      URL = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${recipesInput}`;
-      fetch(URL)
-        .then((r) => r.json())
-        .then((r) => console.log(r));
-      break;
-    case 'Nome':
-      URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${recipesInput}`;
-      fetch(URL)
-        .then((r) => r.json())
-        .then((r) => console.log(r));
-      break;
-    case 'Primeira letra':
-      if (recipesInput.length === 1) {
-        URL = `https://www.themealdb.com/api/json/v1/1/search.php?f=${recipesInput}`;
-        fetch(URL)
-          .then((r) => r.json())
-          .then((r) => console.log(r));
-      } else {
-        alert('Sua busca deve conter somente 1 (um) caracter');
-      }
-      break;
-    default:
-      break;
-    }
-  };
-
-  const handleClickDrink = () => {
-    console.log('Drink');
-    switch (recipesRadio) {
-    case 'Ingrediente':
-      URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${recipesInput}`;
-      fetch(URL)
-        .then((r) => r.json())
-        .then((r) => console.log(r));
-      break;
-    case 'Nome':
-      URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${recipesInput}`;
-      fetch(URL)
-        .then((r) => r.json())
-        .then((r) => console.log(r));
-      break;
-    case 'Primeira letra':
-      if (recipesInput.length === 1) {
-        URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${recipesInput}`;
-        fetch(URL)
-          .then((r) => r.json())
-          .then((r) => console.log(r));
-      } else {
-        alert('Sua busca deve conter somente 1 (um) caracter');
-      }
-      break;
-    default:
-      break;
-    }
-  };
-
   const context = {
     recipesInput,
-    data,
-    handleRadioChange,
+    recipesRadio,
     handleRecipesInput,
-    handleClickFood,
-    handleClickDrink,
+    handleRadioChange,
   };
 
   return (
