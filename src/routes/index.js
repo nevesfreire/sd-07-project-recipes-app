20679fc6206d3bb3bc4dc720576d16c94a7574f3
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2021-01-31 16:25:34 -0300

    Corrigindo a estrutura de Testes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { Home, Login } from '../pages/index';
 
 export default function Routes() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route path="/bebidas" component={ Home } />
-        <Route path="/comidas" component={ Home } />
-      </Switch>
-    </Router>
-
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/bebidas" component={ Home } />
+      <Route path="/comidas" component={ Home } />
+    </Switch>
   );
 }
--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/react';
-import renderWithRedux from './utils/renderWithRedux';
+import renderWIthRouterAndRedux from './utils/renderWIthRouterAndRedux';
 import App from '../App';
 
 const EMAIL_INPUT = 'email-input';
@@ -9,25 +9,25 @@ const LOGIN_SUBMIT_BTN = 'login-submit-btn';
 
 describe('Testes de login', () => {
   it('existe um campo para inserir email', () => {
-    const { getByTestId } = renderWithRedux(<App />);
+    const { getByTestId } = renderWIthRouterAndRedux(<App />);
 
     expect(getByTestId(EMAIL_INPUT)).toBeInTheDocument();
   });
 
   it('existe um campo para inserir senha', () => {
-    const { getByTestId } = renderWithRedux(<App />);
+    const { getByTestId } = renderWIthRouterAndRedux(<App />);
 
     expect(getByTestId(PASSWORD_INPUT)).toBeInTheDocument();
   });
 
   it('botão de envio desativado quando o email e a senha não foram validados', () => {
-    const { getByTestId } = renderWithRedux(<App />);
+    const { getByTestId } = renderWIthRouterAndRedux(<App />);
 
     expect(getByTestId(LOGIN_SUBMIT_BTN)).toHaveAttribute('disabled');
   });
 
   it('o botão de envio está desativado quando apenas o email foi preenchido', () => {
-    const { getByTestId } = renderWithRedux(<App />);
+    const { getByTestId } = renderWIthRouterAndRedux(<App />);
 
     const emailInput = getByTestId(EMAIL_INPUT);
     fireEvent.change(emailInput, { target: { value: 'abcde@gmail.com' } });
@@ -35,7 +35,7 @@ describe('Testes de login', () => {
   });
 
   it('o botão de envio está desativado quando apenas a senha foi preenchido', () => {
-    const { getByTestId } = renderWithRedux(<App />);
+    const { getByTestId } = renderWIthRouterAndRedux(<App />);
 
     const passwordInput = getByTestId(PASSWORD_INPUT);
     fireEvent.change(passwordInput, { target: { value: 'senha012' } });
@@ -43,7 +43,7 @@ describe('Testes de login', () => {
   });
 
   it('o botão de envio está ativado quando a senha e o email foram validados', () => {
-    const { getByTestId } = renderWithRedux(<App />);
+    const { getByTestId } = renderWIthRouterAndRedux(<App />);
 
     const emailInput = getByTestId(EMAIL_INPUT);
     const passwordInput = getByTestId(PASSWORD_INPUT);
