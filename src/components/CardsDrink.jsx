f8bab54c879b1f406b9292e8027864e26d54fe18
MoisesSantana <moisaant@gmail.com>
2021-01-31 20:25:13 -0300

    estruturação tela principal de receitas/bebidas

---
+++ src/components/CardsDrink.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import CoffeeAndCodeContext from '../context/CoffeeAndCodeContext';
+import { requestApiDrinkFilterName } from '../services/requestDrink';
+
+function CardsFood() {
+  const maxCardAmount = 12;
+  const firstCard = 0;
+  const [cardAmount, setCardAmount] = useState(maxCardAmount);
+
+  const { cardDrink, setCardDrink } = useContext(CoffeeAndCodeContext);
+
+  useEffect(() => {
+    if (!cardDrink.length) callApi();
+  }, []);
+
+  const callApi = async () => {
+    const apiResult = await requestApiDrinkFilterName();
+    setCardDrink(apiResult);
+  };
+
+  if (!cardDrink.length) return <span>Loading...</span>;
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+  return (
+    <div>
+      {
+        cardDrink.slice(firstCard, cardAmount)
+          .map((currentObject, index) => {
+            console.log(currentObject)
+            const {
+              idDrink,
+              strDrink,
+              strDrinkThumb,
+            } = currentObject;
+
+            return (
+              <div
+                key={ idDrink }
+                data-testid={ `${index}-recipe-card` }
+              >
+                <Link to={ `/comidas/${idDrink}` }>
+                  <img
+                    src={ strDrinkThumb }
+                    alt={ strDrink }
+                    data-testid={ `${index}-card-img` }
+                  />
+                </Link>
+                <div>
+                  <Link to={ `/comidas/${idDrink}` }>
+                    <h4 data-testid={ `${index}-card-name` }>{ strDrink }</h4>
+                  </Link>
+                </div>
+              </div>
+            );
+          })
+      }
+      <button
+        type="button"
+      >
+        See More
+      </button>
+    </div>
+  );
+}
+
+export default CardsFood;
