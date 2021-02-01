53a4134aa5110b988d5bd1dc956ca8758c2a31d7
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2021-02-01 16:04:32 -0300

    requsito 43

--- src/pages/bebidas/BebidasDetalhes.js
+++ src/pages/bebidas/BebidasDetalhes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Carousel from './CarouselBebida';
+import ShareIcon from '../../images/shareIcon.svg';
 
 const MAX_INGREDIENTS = 20;
 
@@ -9,11 +10,13 @@ export default class BebidasDetalhes extends React.Component {
     super();
     this.state = {
       recipe: {},
+      copyClipboard: '',
     };
     this.fetchData = this.fetchData.bind(this);
     this.renderIngredients = this.renderIngredients.bind(this);
     this.renderIngredient = this.renderIngredient.bind(this);
     this.iniciarReceita = this.iniciarReceita.bind(this);
+    this.copyClipboard = this.copyClipboard.bind(this);
   }
 
   componentDidMount() {
@@ -37,6 +40,14 @@ export default class BebidasDetalhes extends React.Component {
     history.push(`/bebidas/${id}/in-progress`);
   }
 
+  async copyClipboard() {
+    await navigator.clipboard.writeText(window.location.href);
+    const copySucess = await window.location.href;
+    this.setState({
+      copyClipboard: copySucess,
+    });
+  }
+
   renderIngredient(index) {
     const { recipe } = this.state;
     const ingredient = recipe[`strIngredient${index + 1}`];
@@ -58,7 +69,7 @@ export default class BebidasDetalhes extends React.Component {
   }
 
   render() {
-    const { recipe } = this.state;
+    const { recipe, copyClipboard } = this.state;
     return (
       <div>
         <img data-testid="recipe-photo" src={ recipe.strDrinkThumb } alt="drink pic" />
@@ -66,9 +77,13 @@ export default class BebidasDetalhes extends React.Component {
         <button
           type="button"
           data-testid="share-btn"
+          onClick={ this.copyClipboard }
         >
-          Compartilhar
+          <img src={ ShareIcon } alt="compartilhar" />
         </button>
+        <span>
+          {copyClipboard ? 'Link copiado!' : ''}
+        </span>
         <button
           type="button"
           data-testid="favorite-btn"
