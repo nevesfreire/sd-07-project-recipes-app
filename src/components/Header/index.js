4cc33ad3b652b1ae0f30b350122c49c44c85d2c9
berchon <berchon@gmail.com>
2021-02-01 15:27:51 -0300

    Arrumando uma duplicidade na importacao no arquivo Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import SearchBar from '../SearchBar';
 import imageProfile from '../../images/profileIcon.svg';
 import imageSearch from '../../images/searchIcon.svg';
 import SearchBar from '../SearchBar';
