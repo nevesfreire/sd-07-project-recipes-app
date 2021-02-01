f9909f23aa8fac70aeaa4cb03f8ba053e12b5480
Breno Alves <brenoalv3ss@gmail.com>
2021-02-01 16:56:04 -0300

    alterações

--- src/pages/geral/Origem.js
+++ src/pages/geral/Origem.js
@@ -1,19 +1,85 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loadRecipes } from '../../redux/action';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 
 class Origem extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+    this.Meals = this.Meals.bind(this);
+  }
+
+  componentDidMount() {
+    const { loadrecipes } = this.props;
+    loadrecipes('comidas');
+  }
+
+  handleClick(valor) {
+    const { history } = this.props;
+    history.push(`/comidas/${valor}`);
+  }
+
+  Meals() {
+    const { history, receitas } = this.props;
+    let controlealert = false;
+    if (receitas.meals || receitas.meals === null) {
+      if (receitas.meals === null && !controlealert) {
+        controlealert = true;
+        alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+      } else if (receitas.meals.length === 1 && receitas.redirect) {
+        const id = receitas.meals[0].idMeal;
+        history.push(`/comidas/${id}`);
+      } else {
+        return (receitas.meals.map((receita, index) => {
+          const limit = 12;
+          if (index < limit) {
+            return (
+              <button
+                className="card"
+                name={ receita.idMeal }
+                type="button"
+                onClick={ ({ target }) => this.handleClick(target.name) }
+                key={ index }
+                data-testid={ `${index}-recipe-card` }
+              >
+                <img
+                  className="card"
+                  name={ receita.idMeal }
+                  data-testid={ `${index}-card-img` }
+                  src={ receita.strMealThumb }
+                  alt="imagem da receita"
+                />
+                <h1
+                  className="card"
+                  name={ receita.idMeal }
+                  data-testid={ `${index}-card-name` }
+                >
+                  {receita.strMeal}
+                </h1>
+              </button>
+            );
+          }
+          return null;
+        }));
+      }
+    }
+  }
+
   render() {
     const { history, match } = this.props;
     return (
       <div>
         <Header
           title="Explorar Origem"
-          searchOn="on"
           history={ history }
           match={ match }
         />
+        {
+          this.Meals()
+        }
         <Footer history={ history } />
       </div>
     );
@@ -23,6 +89,16 @@ class Origem extends Component {
 Origem.propTypes = {
   history: PropTypes.objectOf({ push: PropTypes.func.isRequired }).isRequired,
   match: PropTypes.objectOf().isRequired,
+  receitas: PropTypes.objectOf().isRequired,
+  loadrecipes: PropTypes.func.isRequired,
 };
 
-export default Origem;
+const mapStateToProps = (state) => ({
+  receitas: state.fastFood.receitas,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loadrecipes: (e) => dispatch(loadRecipes(e)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Origem);
