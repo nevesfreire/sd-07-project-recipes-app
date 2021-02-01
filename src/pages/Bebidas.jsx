84c50b9d7917baccc1e348b9d1e853bffdf235a4
MoisesSantana <moisaant@gmail.com>
2021-02-01 13:30:41 -0300

    correção ESLint

--- src/pages/Bebidas.jsx
+++ src/pages/Bebidas.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import CardsDrink from '../components/CardsDrink'
+import CardsDrink from '../components/CardsDrink';
 
 function Bebidas() {
   return (
