fd10d47e4aef43b87813249caf4bb6590e44cf6f
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2021-02-01 17:40:53 -0300

    Alterada a rota da pagina de perfil

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route exact path="/profile" component={ Profile } />
+      <Route exact path="/perfil" component={ Profile } />
       <Route exact path="/explorar" component={ Explorer } />
       <Route exact path="/comidas" component={ Meals } />
       <Route exact path="/comidas/:id" component={ MealRecipeDetails } />
