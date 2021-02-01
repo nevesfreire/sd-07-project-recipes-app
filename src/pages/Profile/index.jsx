02b1cf3098d57dd3f8e58475128b3bca9882d605
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-02-01 16:28:00 -0300

    requisito 82

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Header, Footer } from '../../components';
+import { RecipesContext } from '../../context';
 
 export default function Profile() {
+  const { login } = useContext(RecipesContext);
+
   return (
     <div>
       <Header title="Perfil" />
+      <h2 data-testid="profile-email">
+        Email:
+        { login.email }
+      </h2>
+      <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+      <button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button>
+      <button type="button" data-testid="profile-logout-btn">Sair</button>
       <Footer />
     </div>
   );
