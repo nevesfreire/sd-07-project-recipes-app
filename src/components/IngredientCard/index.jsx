96f91de1ad874bdaa4ecf7076c5baa15ea889929
Flavio Oliveira <flavsoliver@gmail.com>
2021-02-01 20:00:19 -0300

    implementação de estilização

--- src/components/IngredientCard/index.jsx
+++ src/components/IngredientCard/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import './style.css';
+import './IngredientCard.css';
 
 export default function IngredientCard({ id, ingredient }) {
   const { location: { pathname } } = useHistory();
