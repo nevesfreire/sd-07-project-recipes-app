9038d784d0f992abbd5d25c83c00ca57b37e1661
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2021-02-02 18:05:32 -0300

    inicio dos requisitos 54 e 55

--- src/pages/DoneRecipe.js
+++ src/pages/DoneRecipe.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import '../css/food.css';
+import shareIcon from '../images/shareIcon.svg';
 
 class DoneRecipe extends Component {
   render() {
+    const index = 0;
+    const tagName = 'tagName';
     return (
       <div>
-        Receitas feitas
+        <button type="button" data-testid="filter-by-all-btn">
+          All
+        </button>
+        <button type="button" data-testid="filter-by-food-btn">
+          Food
+        </button>
+        <button type="button" data-testid="filter-by-drink-btn">
+          Drinks
+        </button>
+        <div data-testid={`${index}-horizontal-image`}>
+          <img data-testid={`${index}-horizontal-image`} src={shareIcon} />
+          <h2 data-testid={`${index}-horizontal-top-text`}>Categoria</h2>
+          <p data-testid={`${index}-horizontal-done-date`}>Data da receita</p>
+          <img data-testid={`${index}-horizontal-share-btn`} />
+          <h1 data-testid={`${index}-horizontal-name`}>Nome da receita</h1>
+          <tag data-testid={`${index}-${tagName}-horizontal-tag`}>tag</tag>
+        </div>
       </div>
     );
   }
