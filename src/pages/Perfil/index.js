77018b1ea98b2be6025ee36182541c1bfe4f668a
Galaraz <jvictor.si.2013@gmail.com>
2021-02-02 17:07:54 -0300

    requisitos

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
 import { Container, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
 import { Header, Footer } from '../../components';
 
 class Perfil extends Component {
   render() {
     const title = 'Perfil';
+    const { history } = this.props;
     return (
       <div>
         <Header title={ title } />
         <Container>
-          <h1>email aqui</h1>
+          <h1 data-testid="profile-email">email@email</h1>
         </Container>
         <Container>
-          <Button variant="secondary" size="lg" block>
+          <Button
+            variant="secondary"
+            block
+            size="lg"
+            data-testid="profile-done-btn"
+            onClick={ () => history.push('/receitas-feitas') }
+          >
             Receitas Feitas
           </Button>
-          <Button variant="secondary" size="lg" block>
+          <Button
+            variant="secondary"
+            block
+            size="lg"
+            data-testid="profile-favorite-btn"
+            onClick={ () => history.push('/receitas-favoritas') }
+          >
             Receitas Favoritas
           </Button>
-          <Button variant="secondary" size="lg" block>
+          <Button
+            variant="secondary"
+            block
+            size="lg"
+            data-testid="profile-logout-btn"
+            onClick={ () => history.push('/') }
+          >
             Sair
           </Button>
         </Container>
@@ -27,5 +48,11 @@ class Perfil extends Component {
     );
   }
 }
+
+Perfil.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Perfil;
