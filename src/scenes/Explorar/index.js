5adc48db0804e9c6e897eac4db181e06c116828e
Bruno Dantas <bruno.wesley77@gmail.com>
2021-02-02 17:59:57 -0300

    feat: create Explore page

--- src/scenes/Explorar/index.js
+++ src/scenes/Explorar/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 import Header from '../../common/Header';
 
 export default function index() {
   return (
-    <div>
+    <main>
       <Header />
-    </div>
+      <Link to="/explorar/comidas">
+        <Button
+          data-testid="explore-food"
+          placeholder="Explorar Comidas"
+        >
+          Explorar Comidas
+
+        </Button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <Button
+          data-testid="explore-drinks"
+          placeholder="Bebidas"
+        >
+          Explorar Bebidas
+
+        </Button>
+      </Link>
+    </main>
   );
 }
