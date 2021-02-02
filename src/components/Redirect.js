bcb1827c15b29d9df8eb1a3af12254b0001f08e5
Fernando <nando.neves@gmail.com>
2021-02-02 13:43:10 -0300

    main-group-13-req-54-finalizadas

---
+++ src/components/Redirect.js
@@ -0,0 +1,27 @@
+export default function RedirectToDone(itemId, mealType, details) {
+  const dez = 10;
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() < dez ? `0${today.getMonth()}` : today.getMonth();
+  const day = today.getDate() < dez ? `0${today.getDate()}` : today.getDate();
+  console.log(details.strTags);
+  const item = {
+    id: itemId,
+    type: mealType === 'Meal' ? 'comida' : 'bebida',
+    area: mealType === 'Meal' ? details.strArea : '',
+    category: details.strCategory,
+    alcoholicOrNot: mealType === 'Drink' ? details.strAlcoholic : '',
+    name: details[`str${mealType}`],
+    image: details[`str${mealType}Thumb`],
+    doneDate: `${day}/${month}/${year}`,
+    tags: details.strTags ? (details.strTags).split(',') : [],
+  };
+  // 'strTags': 'Pasta,Curry',
+  const list = JSON.parse(localStorage.getItem('doneRecipes'));
+  if (list) {
+    list.push(item);
+    localStorage.setItem('doneRecipes', list);
+  } else {
+    localStorage.setItem('doneRecipes', JSON.stringify([item]));
+  }
+}
