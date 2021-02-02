cdb2c90927ffcecb7cd98f9966a4f0ba74150b19
Carol Bezerra <carol.arbe@gmail.com>
2021-02-02 18:38:11 -0300

    Requisitos 67, 68 e 69

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 
 export default class Explorar extends React.Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <Header title="Explorar" />
-        Explorar
+        <button
+          type="button"
+          data-testid="explore-food"
+          onClick={ () => history.push('/explorar/comidas') }
+        >
+          Explorar Comidas
+        </button>
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          onClick={ () => history.push('/explorar/bebidas') }
+        >
+          Explorar Bebidas
+        </button>
         <Footer />
       </div>
     );
   }
 }
+
+Explorar.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
