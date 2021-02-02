47ce3ae10d57750e05fecc2528f17c9d6600b656
Arthur de Sa <arthurdesa94@gmail.com>
2021-02-02 17:28:41 -0300

    Branch para o Rufino testar juntoCC1

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
-import { Link } from "react-router-dom";
+
 
 function Profile() {
   const eraseLocalStorage = () => {
@@ -10,35 +11,35 @@ function Profile() {
   const profileItems = () => {
     const { email } = JSON.parse(localStorage.getItem('user'));
     return (
-      <div>
-        <Header title="Profile" />
-        <h4 data-testid="profile-email">{ email }</h4>
-          <Link to="/receitas-feitas">
-            <button
-              type="button"
-              data-testid="profile-done-btn"
-            >
-              Receitas Feitas
-            </button>
-          </Link>
-          <Link to="/receitas-favoritas">
-            <button
-              type="button"
-              data-testid="profile-favorite-btn"
-            >
-              Receitas Favoritas
-            </button>
-          </Link>
-          <Link to="/">
-            <button
-              type="button"
-              onClick={ () => eraseLocalStorage() }
-              data-testid="profile-logout-btn"
-            >
-            Sair
-            </button>
-          </Link>
-          <MenuInferior />
+    <div>
+      <Header title="Profile" />
+      <h4 data-testid="profile-email">{ email }</h4>
+        <Link to="/receitas-feitas">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Receitas Feitas
+          </button>
+        </Link>
+        <Link to="/receitas-favoritas">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Receitas Favoritas
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            onClick={ () => eraseLocalStorage() }
+            data-testid="profile-logout-btn"
+          >
+          Sair
+          </button>
+        </Link>
+        <MenuInferior />
       </div>
     );
   };
