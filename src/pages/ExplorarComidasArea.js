cf0017f667747d98c6c607ebac60c8dc1b78cb4c
vanderson-henrique <vhbenedito@gmail.com>
2021-02-03 17:22:52 -0300

    Tela de Explorar Área

--- src/pages/ExplorarComidasArea.js
+++ src/pages/ExplorarComidasArea.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import FoodsArea from '../Components/FoodsArea';
 
 function ExplorarComidasArea() {
   const isTrue = true;
   return (
     <div>
       <Header text="Explorar Origem" search={ isTrue } />
-      <p>AQUI É Explorar por área</p>
+      <FoodsArea />
       <Footer />
     </div>
   );
