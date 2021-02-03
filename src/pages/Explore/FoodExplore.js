a3e62446123b4153a67b83c99105997e7589b93b
Macarena Rodriguez <macarenar697@gmail.com>
2021-02-03 16:15:07 -0300

    Tela de explorar bebidas ou comidas

--- src/pages/Explore/FoodExplore.js
+++ src/pages/Explore/FoodExplore.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import getMeals from '../../services/mealAPI';
 
 export default function FoodExplore() {
+  const history = useHistory();
+  async function randomRecipes() {
+    const data = await getMeals('random', '');
+    const { idMeal } = data.meals[0];
+    const newMeal = (history.location.pathname)
+      .replace('explorar/comidas', `comidas/${idMeal}`);
+    history.push(newMeal);
+  }
+
   return (
     <div>
-      FoodExplore
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explorar/comidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-area"
+        onClick={ () => history.push('/explorar/comidas/area') }
+      >
+        Por Local de Origem
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ randomRecipes }
+      >
+        Me Surpreenda!
+      </button>
     </div>
   );
 }
