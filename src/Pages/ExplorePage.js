e5b754f574bc83671f5419593f7e4d9f882a6073
rodrigoderkian <derkianpromo@gmail.com>
2021-02-03 16:11:37 -0300

    pagina explorar

--- src/Pages/ExplorePage.js
+++ src/Pages/ExplorePage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import FooterMenu from '../components/FooterMenu';
+import Header from '../components/Header';
 
 function ExplorePage() {
   return (
     <div>
+      <Header />
+      <div>
+        <Link to="/explorar/comidas">
+          <button data-testid="explore-food" type="button">Explorar Comidas</button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          <button data-testid="explore-drinks" type="button">Explorar Bebidas</button>
+        </Link>
+      </div>
       <FooterMenu />
     </div>
   );
