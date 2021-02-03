c581f7cc29cbb04ef68fc012e337e2c85074a1f7
Fernando <nando.neves@gmail.com>
2021-02-02 23:05:15 -0300

    main-group-13-req-78-explorar

--- src/services/API.js
+++ src/services/API.js
@@ -1,69 +1,69 @@
-export async function fetchGlobalMeal() {
+async function fetchGlobalMeal() {
   const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
   const meals = await response.json();
   return meals.meals;
 }
 
-export async function fetchMealCategory() {
+async function fetchMealCategory() {
   const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
   const categories = await response.json();
   return categories.meals;
 }
 
-export async function fetchMealByIngredients(ingredient) {
+async function fetchMealByIngredients(ingredient) {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
   const meals = await response.json();
   return meals.meals;
 }
 
-export async function fetchMealByName(name) {
+async function fetchMealByName(name) {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
   const meals = await response.json();
   return meals.meals;
 }
 
-export async function fetchMealByFirstLetter(letter) {
+async function fetchMealByFirstLetter(letter) {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`);
   const meals = await response.json();
   return meals.meals;
 }
 
-export async function fetchMealByCategory(category) {
+async function fetchMealByCategory(category) {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
   const meals = await response.json();
   return meals.meals;
 }
 
-export async function fetchGlobalDrink() {
+async function fetchGlobalDrink() {
   const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
   const drinks = await response.json();
   return drinks.drinks;
 }
 
-export async function fetchDrinkCategory() {
+async function fetchDrinkCategory() {
   const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
   const categories = await response.json();
   return categories.drinks;
 }
 
-export async function fetchDrinkByIngredients(ingredient) {
+async function fetchDrinkByIngredients(ingredient) {
   const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
   const drinks = await response.json();
   return drinks.drinks;
 }
 
-export async function fetchDrinkByName(name) {
+async function fetchDrinkByName(name) {
   const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
   const drinks = await response.json();
   return drinks.drinks;
 }
 
-export async function fetchDrinksByFirstLetter(letter) {
+async function fetchDrinksByFirstLetter(letter) {
   const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`);
   const drinks = await response.json();
   return drinks.drinks;
 }
-export async function fetchDrinkByCategory(category) {
+async function fetchDrinkByCategory(category) {
   try {
     const response1 = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
     const drinks = await response1.json();
@@ -73,38 +73,59 @@ export async function fetchDrinkByCategory(category) {
   }
 }
 
-export async function fetchDrinkDetailsById(id) {
+async function fetchDrinkDetailsById(id) {
   const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
   const details = await response.json();
   return details.drinks;
 }
 
-export async function fetchFoodDetailsById(id) {
+async function fetchFoodDetailsById(id) {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
   const details = await response.json();
   if (details) return details.meals;
 }
 
-export async function fetchRamdonFood() {
+async function fetchRamdonFood() {
   const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
   const meal = await response.json();
   if (meal) return meal.meals;
 }
 
-export async function fetchRamdonDrink() {
+async function fetchRamdonDrink() {
   const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
   const drink = await response.json();
   if (drink) return drink.drinks;
 }
 
-export async function fetchAllAreaOptions() {
+async function fetchAllAreaOptions() {
   const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
   const list = await response.json();
   if (list) return list.meals;
 }
 
-export async function fetchFoodByArea(area) {
+async function fetchFoodByArea(area) {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`);
   const list = await response.json();
   if (list) return list.meals;
 }
+
+export {
+  fetchGlobalMeal,
+  fetchMealCategory,
+  fetchMealByIngredients,
+  fetchMealByName,
+  fetchMealByFirstLetter,
+  fetchMealByCategory,
+  fetchGlobalDrink,
+  fetchDrinkCategory,
+  fetchDrinkByIngredients,
+  fetchDrinkByName,
+  fetchDrinksByFirstLetter,
+  fetchDrinkByCategory,
+  fetchDrinkDetailsById,
+  fetchFoodDetailsById,
+  fetchRamdonFood,
+  fetchRamdonDrink,
+  fetchAllAreaOptions,
+  fetchFoodByArea,
+};
