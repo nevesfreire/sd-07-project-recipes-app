91d2b3ad66cb1c29b1ba95a6aef9c8d9d295b251
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-02-03 18:39:17 -0300

    Footer

--- src/App.js
+++ src/App.js
@@ -15,26 +15,29 @@ import DrinkIngredient from './pages/DrinkIngredient';
 import FoodArea from './pages/FoodArea';
 import FavoriteRecipes from './pages/FavoriteRecipes';
 
-
 function App() {
   const stringPath = '/explorar/bebidas/ingredientes';
   return (
     <Provider>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/comidas" component={FoodRecipes} />
-          <Route exact path="/bebidas" component={DrinkRecipes} />
-          <Route path="/comidas/:id" component={FoodDetails} />
-          <Route path="/bebidas/:id" component={DrinkDetails} />
-          <Route exact path="/perfil" component={Profile} />
-          <Route exact path="/explorar" component={Explore} />
-          <Route exact path="/explorar/comidas" component={ExploreFood} />
-          <Route exact path="/explorar/bebidas" component={ExploreDrink} />
-          <Route exact path="/explorar/comidas/ingredientes" component={FoodIngredient} />
-          <Route exact path={stringPath} component={DrinkIngredient} />
-          <Route exact path="/explorar/comidas/area" component={FoodArea} />
-          <Route exact path="/receitas-favoritas" component={FavoriteRecipes} />
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/comidas" component={ FoodRecipes } />
+          <Route exact path="/bebidas" component={ DrinkRecipes } />
+          <Route path="/comidas/:id" component={ FoodDetails } />
+          <Route path="/bebidas/:id" component={ DrinkDetails } />
+          <Route exact path="/perfil" component={ Profile } />
+          <Route exact path="/explorar" component={ Explore } />
+          <Route exact path="/explorar/comidas" component={ ExploreFood } />
+          <Route exact path="/explorar/bebidas" component={ ExploreDrink } />
+          <Route
+            exact
+            path="/explorar/comidas/ingredientes"
+            component={ FoodIngredient }
+          />
+          <Route exact path={ stringPath } component={ DrinkIngredient } />
+          <Route exact path="/explorar/comidas/area" component={ FoodArea } />
+          <Route exact path="/receitas-favoritas" component={ FavoriteRecipes } />
         </Switch>
       </BrowserRouter>
     </Provider>
--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import App from '../App';
-import renderWithRouter from '../renderWithRouter'
+import renderWithRouter from '../renderWithRouter';
 
 describe('Tela de Login', () => {
   renderWithRouter(<App />);
   const inputEmail = screen.getByTestId('email-input');
   const inputPassword = screen.getByTestId('password-input');
-  const buttonEnter = screen.getByTestId('login-submit-btn')
+  const buttonEnter = screen.getByTestId('login-submit-btn');
   test('Se existe os elementos esperados na tela', () => {
     expect(inputEmail).toBeInTheDocument();
     expect(inputPassword).toBeInTheDocument();
     expect(buttonEnter).toBeInTheDocument();
-  })
-})
+  });
+});
