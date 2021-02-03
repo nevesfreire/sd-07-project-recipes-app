8c9f76b21bb97e8c0309125969b5b88f43dd6804
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2021-02-03 17:25:18 -0300

    algumas coisinhas so a mais

--- src/Pages/FavoritesRecipes.js
+++ src/Pages/FavoritesRecipes.js
@@ -1,15 +1,17 @@
 import React, { useState, useFetch } from 'react';
+import PropTypes, { func } from 'prop-types';
 import Card from '../components/Card';
 import Header from '../components/Header';
-// import useFetch from '../hooks/useFetch';
 
 function FavoritesRecipes() {
-//   const { randomDrinkFetch, randomFoodFetch } = useFetch();
+//   function handleFilterAll() {
+
+//   }
 
   return (
     <div>
       <Header title="Receitas Favoritas" explore />
-      <button type="button" onClick={ () => {} } data-testid="filter-by-all-btn">All</button>
+      <button type="button" onClick={ () => { } } data-testid="filter-by-all-btn">All</button>
       <button type="button" onClick={ () => {} } data-testid="filter-by-food-btn">Food</button>
       <button type="button" onClick={ () => {} } data-testid="filter-by-drink-btn">Drinks</button>
       <Card />
@@ -17,5 +19,10 @@ function FavoritesRecipes() {
 
   );
 }
+
+FavoritesRecipes.propTypes = {
+  handleFilterAll: PropTypes.func.isRequired,
+
+};
 
 export default FavoritesRecipes;
