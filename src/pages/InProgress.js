53bfc704ec42c5e2253455c3fd0fe0ca09eca9e8
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-02-03 17:25:51 -0300

    Desenvolvendo tela inProgress

--- src/pages/InProgress.js
+++ src/pages/InProgress.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class MainRecipes extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      recipes: [],
+    };
+    this.getRecipeMeal = this.getRecipeMeal.bind(this);
+  }
+
+  async componentDidMount() {
+    const { location: { pathname } } = this.props;
+    const { recipes } = this.state;
+    if (pathname === `/comidas/${recipes[0].idMeal}/in-progress`) {
+      await this.getRecipeMeal();
+  }
+
+  async getRecipeMeal() {
+    const { inProgressRecipes } = this.props;
+    const recipes = await inProgressRecipes;
+    this.setState({ recipes });
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +32,8 @@ class MainRecipes extends React.Component {
   }
 }
 
-export default MainRecipes;
+const mapStateToProps = ({ recipes: { inProgressRecipes } }) => (
+  { inProgressRecipes }
+);
+
+export default connect(mapStateToProps)(MainRecipes);
