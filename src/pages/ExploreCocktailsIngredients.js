c50f094902e325dc1b2012b2a2b75a12f52328ea
brendalima <brenda.c.lima@poli.ufrj.br>
2021-02-03 15:17:55 -0300

    ajustando requisitos anteriores

--- src/pages/ExploreCocktailsIngredients.js
+++ src/pages/ExploreCocktailsIngredients.js
@@ -1,12 +1,12 @@
-/* import React, { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import allActions from '../actions';
-/* import { drinkIngredients, drinkByIngredient } from '../services/cocktailAPI'; */
+import { drinkIngredients, drinkByIngredient } from '../services/cocktailAPI';
 
-/* function ExploreCocktailsIngredients() {
+function ExploreCocktailsIngredients() {
   const dispatch = useDispatch();
   const state = useSelector(({ mainpage }) => mainpage);
   const { isLoading, ingredients } = state;
@@ -66,4 +66,4 @@ import allActions from '../actions';
 }
 
 export default ExploreCocktailsIngredients;
- */
+ 
