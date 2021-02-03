325c0bc97eff72f5b7d7520e9a3b61d5ff232118
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-02-03 14:22:42 -0300

    cjhanges

--- src/components/InputsLogin.jsx
+++ src/components/InputsLogin.jsx
@@ -4,47 +4,46 @@ import context from '../contextAPI/context';
 // import stateChanges from '../helpers/stateChanges';
 import '../css/login.css';
 
+const inputText = (onChange) => (
+  <div className="input-text">
+    <Input
+      data-testid="email-input"
+      type="email"
+      name="user"
+      placeholder="email@email.com"
+      onChange={ (e) => onChange(e) }
+    />
+  </div>
+);
 
-  const inputText = (onChange) => (
-    <div className="input-text">
-      <Input
-        data-testid="email-input"
-        type="email"
-        name="user"
-        placeholder="email@email.com"
-        onChange={ (e) => onChange(e) }
-      />
-    </div>
-  );
+const inputPasswd = (onChange) => (
+  <div className="input-senha">
+    <Input
+      data-testid="password-input"
+      type="password"
+      name="passwd"
+      placeholder="sua senha aqui"
+      onChange={ (e) => onChange(e) }
+    />
+  </div>
+);
 
-  const inputPasswd = (onChange) => (
-    <div className="input-senha">
-      <Input
-        data-testid="password-input"
-        type="password"
-        name="passwd"
-        placeholder="sua senha aqui"
-        onChange={ (e) => onChange(e) }
-      />
-    </div>
-  );
-
-  const buttonLogin = () => (
-    <div className="input-button">
-      <Button
-        data-testid="login-submit-btn"
-        variant="contained"
-        collor="primary"
-        // disable={ xablau }
-        // onClick={ }
-      >
-        Logar
-      </Button>
-    </div>
-  );
+const buttonLogin = () => (
+  <div className="input-button">
+    <Button
+      data-testid="login-submit-btn"
+      variant="contained"
+      collor="primary"
+      // disable={ xablau }
+      // onClick={ }
+    >
+      Logar
+    </Button>
+  </div>
+);
 export default function Render() {
-  const { login, setLogin} = useContext(context);
-  
+  const { login, setLogin } = useContext(context);
+
   const loginChanges = ({ target: { name, value } }) => {
     setLogin({ ...login, [name]: value });
   };
