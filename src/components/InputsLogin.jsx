6533e83903ac2a1731d76c74ddf352b284a8a486
jvitorof <j.vitoroflores@gmail.com>
2021-02-03 01:20:33 -0300

    LOGIN: stateChange

--- src/components/InputsLogin.jsx
+++ src/components/InputsLogin.jsx
@@ -1,37 +1,30 @@
 import React, { useContext } from 'react';
 import { Input, Button } from '@material-ui/core';
 import context from '../contextAPI/context';
-import UseChange from '../helpers/useChanges';
+// import UseChange from '../helpers/useChanges';
 import '../css/login.css';
 
-export default function Render() {
-  const { state, setState } = useContext(context);
 
-  // const HandleStateChange = ({ target: { name, value } }) => {
-  //   // const { state, setState } = useContext(context);
-  //   setState({ ...state, [name]: value });
-  // };
-
-  const inputText = () => (
+  const inputText = (stateChange) => (
     <div className="input-text">
       <Input
         data-testid="email-input"
         type="email"
         name="user"
         placeholder="email@email.com"
-        onChange={ (e) => UseChange(e) }
+        onChange={ (e) => stateChange(e) }
       />
     </div>
   );
 
-  const inputPasswd = () => (
+  const inputPasswd = (stateChange) => (
     <div className="input-senha">
       <Input
         data-testid="password-input"
         type="password"
         name="passwd"
         placeholder="sua senha aqui"
-        onChange={ (e) => UseChange(e) }
+        onChange={ (e) => stateChange(e) }
       />
     </div>
   );
@@ -49,11 +42,17 @@ export default function Render() {
       </Button>
     </div>
   );
+export default function Render() {
+  const { state, setState } = useContext(context);
+  
+  const stateChange = ({ target: { name, value } }) => {
+    setState({ ...state, [name]: value });
+  };
 
   return (
     <div>
-      {inputText()}
-      {inputPasswd()}
+      {inputText(stateChange)}
+      {inputPasswd(stateChange)}
       {buttonLogin()}
     </div>
   );
--- src/provider/index.jsx
+++ src/provider/index.jsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Context from '../contextAPI/context';
-import HandleStateChange from './helpers/HandleStateChange';
+// import HandleStateChange from './helpers/HandleStateChange';
 
 function Provider({ children }) {
   const [state, setState] = useState({});
 
-  useEffect(() => {
-    UseChanges();
-  }, [state]);
+  // useEffect(() => {
+  //   UseChanges();
+  // }, [state]);
 
   const context = {
     state,
     setState,
-    HandleStateChange,
+    // HandleStateChange,
   };
 
   return (
