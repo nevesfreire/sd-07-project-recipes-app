1cd210c8bea256652b9cb7e9e1d51afd44654bf5
MoisesSantana <moisaant@gmail.com>
2021-02-03 00:34:55 -0300

    chamada da Api principal

--- src/pages/DetalhesReceita.jsx
+++ src/pages/DetalhesReceita.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { requestApiFoodDetails } from '../services/requestFood';
 
-function DetalhesReceitas() {
-  return <div>oi</div>;
+function DetalhesReceitas({ match: { params: { id } } }) {
+  const [foodDetails, setFoodDetails] = useState([]);
+
+  const callMainApi = async () => {
+    const apiResult = await requestApiFoodDetails(id);
+    setFoodDetails(apiResult);
+  };
+
+  useEffect(() => {
+    callMainApi();
+  }, []);
+
+  return (
+    <div>
+      oi
+    </div>
+  );
 }
+
+DetalhesReceitas.propTypes = { id: PropTypes.number.isRequired };
 
 export default DetalhesReceitas;
