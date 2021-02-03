862c7911a57e9a2e4febb8c53eaaadcf733cb580
vitorAmorims <vtamorims@gmail.com>
2021-02-03 18:26:53 -0300

    funçoes handleImage e handleToogle

--- src/pages/DrinkProcess.jsx
+++ src/pages/DrinkProcess.jsx
@@ -1,23 +1,24 @@
 import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import GlobalContext from '../context/GlobalContext';
 import RecipeDetailsContext from '../context/RecipeContext';
 import likeIcon from '../images/whiteHeartIcon.svg';
 import fullLikeIcon from '../images/blackHeartIcon.svg';
 import ShareButton from '../components/ShareButton';
-import ShowRecommended from '../components/RecommendedFoodOrDrinks';
 import './foodAndDrinkDetails.css';
 import {
   ingredientsMount,
   unLikeRecipe,
   setLikeImage,
-  saveFavoriteRecipe,
 } from '../components/func_details';
 
-function DrinkProcess() {
+function DrinkProcess(props) {
   const contextGlobal = useContext(GlobalContext);
   const { setTitle } = contextGlobal;
   const context = useContext(RecipeDetailsContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [inProgressRecipes, setInProgressRecipes] = useState(true);
   const [btnImg, setBtnImg] = useState('');
   const { 
     getRecipeTitle,
@@ -34,9 +35,111 @@ function DrinkProcess() {
     setRecipeInstructions,
     setRecipeTags,
     getRecipeTags,
-     } = context;
+  } = context;
+  const { match, history: { location: { pathname } } } = props;
+  const { params } = match;
+  const { id } = params;
+
+  const fetchRecipe = async () => {
+    const path = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+    const getRecipe = await fetch(path);
+    const jsonRecipe = await getRecipe.json();
+    setRecipeTitle(jsonRecipe.drinks[0].strDrink);
+    setRecipeCategory(jsonRecipe.drinks[0].strCategory);
+    setRecipeImage(jsonRecipe.drinks[0].strDrinkThumb);
+    setRecipeInstructions(jsonRecipe.drinks[0].strInstructions);
+    setRecipeAlc(jsonRecipe.drinks[0].strAlcoholic);
+    ingredientsMount(jsonRecipe);
+    setIsLoading(false);
+  };
+
+  const handleImage = () => {
+    if (btnImg === likeIcon) {
+      setBtnImg(fullLikeIcon);
+      saveFavoriteRecipe();
+    } else {
+      setBtnImg(likeIcon);
+      unLikeRecipe();
+    }
+  };
+
+  const handleToogle = (item) => {
+    if (localStorage.getItem('inProgressRecipes')) {
+      const prevLocalStorage = JSON
+        .parse(localStorage.getItem('inProgressRecipes'));
+      console.log(prevLocalStorage);
+      const onHere = prevLocalStorage.meals[id]
+        .find((currentItem) => currentItem === item);
+      if (onHere) {
+        return 'is-checked';
+      }
+    }
+    return 'is-not-checked';
+  };
+
+  useEffect(() => {
+    setTitle('Drink In Progress');
+    fetchRecipe();
+    setLikeImage();
+  }, [])
   return(
-    <h1>Olá eu sou receita em progresso Drink</h1>
+    <div className="recipe-details-container">
+      <img
+        src={ getRecipeImage }
+        alt={ getRecipeTitle }
+        data-testid="recipe-photo"
+        className="recipe-details-image"
+      />
+      <h1 data-testid="recipe-title" className="recipe-details-name">
+        { getRecipeTitle }
+      </h1>
+      <div className="favorite-and-share-btn-container">
+        <button type="button" onClick={ handleImage } className="favorite-btn">
+          <img src={ btnImg } alt="like" data-testid="favorite-btn" />
+        </button>
+        <ShareButton path={ pathname } />
+      </div>
+      <h3 className="recipe-in-progress-category">
+        Category-
+        <span data-testid="recipe-category">{getRecipeCategory}</span>
+      </h3>
+      <ul className="ingredients-checklist">
+      { !isLoading && getRecipeIngredients.map((item, index) => (
+          <li
+            key={ item }
+            data-testid={ `${index}-ingredient-step` }
+          >
+            <label
+              htmlFor={ item }
+              className={ handleToogle(item) }
+            >
+              <input
+                type="checkbox"
+                checked={ handleCheckedFromLocalStorage(item) }
+                name={ item }
+                id={ item }
+                onChange={ handleChecked }
+              />
+              { item }
+            </label>
+          </li>
+        ))}
+      </ul>
+      <p data-testid="instructions" className="recipe-details-instructions">
+        {getRecipeInstructions}
+      </p>
+      <Link to="/receitas-feitas">
+        <button
+          type="button"
+          data-testid="finish-recipe-btn"
+          className="finish-recipe-btn"
+          disabled={ handleFinishRecipe(getRecipeIngredients.length) }
+          onClick={ handleDoneLocalStorage }
+        >
+          Finalizar receita
+        </button>
+      </Link>
+    </div>
   )
 }
 
--- src/pages/FoodProcess.jsx
+++ src/pages/FoodProcess.jsx
@@ -1,11 +1,11 @@
 import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import GlobalContext from '../context/GlobalContext';
 import RecipeDetailsContext from '../context/RecipeContext';
 import likeIcon from '../images/whiteHeartIcon.svg';
 import fullLikeIcon from '../images/blackHeartIcon.svg';
 import ShareButton from '../components/ShareButton';
-import ShowRecommended from '../components/RecommendedFoodOrDrinks';
 import './foodAndDrinkDetails.css';
 import {
   ingredientsMount,
@@ -14,10 +14,12 @@ import {
   saveFavoriteRecipe,
 } from '../components/func_details';
 
-function FoodProcess() {
+function FoodProcess(props) {
   const contextGlobal = useContext(GlobalContext);
   const { setTitle } = contextGlobal;
   const context = useContext(RecipeDetailsContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [inProgressRecipes, setInProgressRecipes] = useState(true);
   const [btnImg, setBtnImg] = useState('');
   const { 
     getRecipeTitle,
@@ -34,9 +36,120 @@ function FoodProcess() {
     setRecipeInstructions,
     setRecipeTags,
     getRecipeTags,
-     } = context;
+  } = context;
+  const { match, history: { location: { pathname } } } = props;
+  const { params } = match;
+  const { id } = params;
+
+  const fetchRecipe = async () => {
+    // const path = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771';
+    const path = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    const getRecipe = await fetch(path);
+    const result = await getRecipe.json();
+    // console.log(result); chave meals interesse
+    setRecipeTitle(result.meals[0].strMeal);
+    setRecipeCategory(result.meals[0].strCategory);
+    setRecipeImage(result.meals[0].strMealThumb);
+    setRecipeInstructions(result.meals[0].strInstructions);
+    setRecipeArea(result.meals[0].strArea);
+    ingredientsMount(setRecipeIngredients, result);
+    setIsLoading(false);
+  };
+
+  const handleImage = () => {
+    const options = {
+      id,
+      getRecipeArea,
+      getRecipeCategory,
+      getRecipeTitle,
+      getRecipeImage,
+    };
+    if (btnImg === likeIcon) {
+      setBtnImg(fullLikeIcon);
+      saveFavoriteRecipe(options);
+    } else {
+      setBtnImg(likeIcon);
+      unLikeRecipe(id);
+    }
+  };
+
+  const handleToogle = (item) => {
+    if (localStorage.getItem('inProgressRecipes')) {
+      const prevLocalStorage = JSON
+        .parse(localStorage.getItem('inProgressRecipes'));
+      console.log(prevLocalStorage);
+      const onHere = prevLocalStorage.meals[id]
+        .find((currentItem) => currentItem === item);
+      if (onHere) {
+        return 'is-checked';
+      }
+    }
+    return 'is-not-checked';
+  };
+
+  useEffect(() => {
+    setTitle('Food In Progress');
+    fetchRecipe();
+    setLikeImage();
+  }, [])
   return(
-    <h1>Olá eu sou receita em progresso Food</h1>
+    <div className="recipe-details-container">
+      <img
+        src={ getRecipeImage }
+        alt={ getRecipeTitle }
+        data-testid="recipe-photo"
+        className="recipe-details-image"
+      />
+      <h1 data-testid="recipe-title" className="recipe-details-name">
+        { getRecipeTitle }
+      </h1>
+      <div className="favorite-and-share-btn-container">
+        <button type="button" onClick={ handleImage } className="favorite-btn">
+          <img src={ btnImg } alt="like" data-testid="favorite-btn" />
+        </button>
+        <ShareButton path={ pathname } />
+      </div>
+      <h3 className="recipe-in-progress-category">
+        Category-
+        <span data-testid="recipe-category">{getRecipeCategory}</span>
+      </h3>
+      <ul className="ingredients-checklist">
+      { !isLoading && getRecipeIngredients.map((item, index) => (
+          <li
+            key={ item }
+            data-testid={ `${index}-ingredient-step` }
+          >
+            <label
+              htmlFor={ item }
+              className={ handleToogle(item) }
+            >
+              <input
+                type="checkbox"
+                checked={ handleCheckedFromLocalStorage(item) }
+                name={ item }
+                id={ item }
+                onChange={ handleChecked }
+              />
+              { item }
+            </label>
+          </li>
+        ))}
+      </ul>
+      <p data-testid="instructions" className="recipe-details-instructions">
+        {getRecipeInstructions}
+      </p>
+      <Link to="/receitas-feitas">
+        <button
+          type="button"
+          data-testid="finish-recipe-btn"
+          className="finish-recipe-btn"
+          disabled={ handleFinishRecipe(getRecipeIngredients.length) }
+          onClick={ handleDoneLocalStorage }
+        >
+          Finalizar receita
+        </button>
+      </Link>
+    </div>
   )
 }
 
