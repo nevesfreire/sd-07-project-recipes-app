2c02c2933c480011fc306af065e154070e34fe66
Luiz Mariz <lfsmariz@gmail.com>
2021-02-03 19:14:22 -0300

    create completeScreen

---
+++ src/Components/CompleteScreen/index.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import CardComplete from '../CardComplete';
+
+const CompleteScreen = () => {
+  const [arrDone, setArrDone] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const acessStorage = () => {
+    const stringDone = localStorage.getItem('doneRecipes');
+    const jsonDone = stringDone !== null ? JSON.parse(stringDone) : [];
+    return jsonDone;
+  };
+
+  useEffect(() => {
+    const doneRecipes = acessStorage();
+    setArrDone(doneRecipes);
+  }, []);
+
+  const arrFilter = arrDone.filter((elem) => elem.type.includes(filter));
+
+  return (
+    <>
+      <button type="button" data-testid="filter-by-all-btn" onClick={ () => setFilter('') }>
+        All
+      </button>
+      <button type="button" data-testid="filter-by-food-btn" onClick={ () => setFilter('comida') }>
+        Food
+      </button>
+      <button type="button" data-testid="filter-by-drink-btn" onClick={ () => setFilter('bebida') }>
+        Drinks
+      </button>
+      {arrFilter.map((elem, index) => (
+        <CardComplete
+          key={ elem.name }
+          num={ index }
+          img={ elem.image }
+          category={ elem.category }
+          nameRecipe={ elem.name }
+          date={ elem.doneDate }
+          tags={ elem.tags }
+          area={ elem.area }
+          alcoholicOrNot={ elem.alcoholicOrNot }
+          type={ elem.type }
+          id={ elem.id }
+        />
+      ))}
+    </>
+  );
+};
+
+export default CompleteScreen;
