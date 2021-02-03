198a83e4c63eccdd5cdeb3719a929b851b62c498
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2021-02-03 12:08:47 -0300

    ajustes de layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,55 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { profileIcon } from '../../images/index';
+import { useHistory } from 'react-router-dom';
+import { profileIcon, searchIcon } from '../../images';
+
 import SearchBar from '../SearchBar';
 
+import { StyledCard, StyledAccordion, StyledImage, StyledTitle } from './styles';
+
 function Header(props) {
+  const history = useHistory();
+
+  function handleClick() {
+    history.push('/perfil');
+  }
   const [propsState] = useState(props);
 
   const { title, showSearchBar } = propsState;
 
   return (
-    <header>
-      <nav>
-        <Link to="/perfil">
-          <img
+    <StyledAccordion>
+      <StyledCard>
+        <StyledCard.Header>
+          <StyledImage
             data-testid="profile-top-btn"
             src={ profileIcon }
             alt="Icone de perfil"
+            onClick={ handleClick }
           />
-        </Link>
-        <h1
-          data-testid="page-title"
-        >
-          { title }
-        </h1>
-        { showSearchBar && <SearchBar /> }
-      </nav>
-    </header>
+          <StyledTitle
+            data-testid="page-title"
+          >
+            { title }
+          </StyledTitle>
+          {
+            showSearchBar
+            && (
+              <StyledAccordion.Toggle
+                as={ StyledImage }
+                eventKey="0"
+                src={ searchIcon }
+                alt="Search"
+                data-testid="search-top-btn"
+              />
+
+            )
+          }
+        </StyledCard.Header>
+        <StyledAccordion.Collapse eventKey="0">
+          <StyledCard.Body><SearchBar /></StyledCard.Body>
+        </StyledAccordion.Collapse>
+      </StyledCard>
+    </StyledAccordion>
   );
 }
 
