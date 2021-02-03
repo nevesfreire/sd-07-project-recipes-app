038968bab931952cb3c53583c991446bc077cd93
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2021-02-03 09:23:31 -0300

    requisito 47

--- src/pages/bebidas/ProgressoBebidas.js
+++ src/pages/bebidas/ProgressoBebidas.js
@@ -1,7 +1,132 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ShareIcon from '../../images/shareIcon.svg';
+import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../../images/blackHeartIcon.svg';
 
-export default class ProgressoComida extends React.Component {
+const MAX_INGREDIENTS = 20;
+
+export default class ProgressoBebidas extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      recipe: {},
+      copyClipboard: '',
+      isFavorite: false,
+    };
+    this.fetchData = this.fetchData.bind(this);
+    this.renderIngredients = this.renderIngredients.bind(this);
+    this.renderIngredient = this.renderIngredient.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const { match: { params } } = this.props;
+    const { id } = params;
+    const responseAPI = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const recipe = await responseAPI.json();
+    this.setState({
+      recipe: recipe.drinks[0],
+    });
+  }
+
+  async copyClipboard() {
+    try {
+      console.log('copy clipboard');
+      await navigator.clipboard.writeText(window.location.href);
+      console.log('copy clipboard write done');
+      this.setState({
+        copyClipboard: window.location.href,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  favoriteRecipe() {
+    const { isFavorite } = this.state;
+    this.setState({
+      isFavorite: !isFavorite,
+    });
+  }
+
+  renderIngredients() {
+    return [...Array(MAX_INGREDIENTS)]
+      .map((_, index) => this.renderIngredient(index));
+  }
+
+  renderIngredient(index) {
+    const { recipe } = this.state;
+    const ingredient = recipe[`strIngredient${index + 1}`];
+    const measure = recipe[`strMeasure${index + 1}`];
+    if (!ingredient || !measure) {
+      return;
+    }
+    const description = `${measure} of ${ingredient}`;
+    return (
+      <li data-testid={ `${index}-ingredient-step` }>
+        {description}
+      </li>
+    );
+  }
+
   render() {
-    return (<div> ProcessoBebidas</div>);
+    const { recipe, copyClipboard, isFavorite } = this.state;
+    console.log(recipe);
+    return (
+      <div>
+        <img
+          data-testid="recipe-photo"
+          src={ recipe.strDrinkThumb }
+          alt="imagem da receita"
+          width="200px"
+        />
+        <h1 data-testid="recipe-title">{recipe.strDrink}</h1>
+        <div>
+          <button
+            type="button"
+            data-testid="share-btn"
+            onClick={ this.copyClipboard }
+          >
+            <img src={ ShareIcon } alt="compartilhar" />
+          </button>
+        </div>
+        <span>
+          {copyClipboard ? 'Link copiado!' : ''}
+        </span>
+        <div>
+          <button
+            type="button"
+            data-testid="favorite-btn"
+            onClick={ () => this.favoriteRecipe() }
+            src={ !isFavorite ? whiteHeartIcon : blackHeartIcon }
+          >
+            <img src={ !isFavorite ? whiteHeartIcon : blackHeartIcon } alt="favorite" />
+          </button>
+        </div>
+        <p data-testid="recipe-category">{recipe.strAlcoholic}</p>
+        <ul>{this.renderIngredients()}</ul>
+        <p data-testid="instructions">{recipe.strInstructions}</p>
+
+        <button
+          type="button"
+          data-testid="finish-recipe-btn"
+        >
+          Favoritar
+        </button>
+      </div>
+    );
   }
 }
+
+ProgressoBebidas.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.string,
+  }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
--- src/pages/comidas/ProgressoComida.js
+++ src/pages/comidas/ProgressoComida.js
@@ -1,28 +1,116 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ShareIcon from '../../images/shareIcon.svg';
+import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../../images/blackHeartIcon.svg';
+
+const MAX_INGREDIENTS = 20;
 
 class ProgressoComida extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      recipe: {},
+      copyClipboard: '',
+      isFavorite: false,
+    };
+    this.fetchData = this.fetchData.bind(this);
+    this.renderIngredients = this.renderIngredients.bind(this);
+    this.renderIngredient = this.renderIngredient.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const { match: { params } } = this.props;
+    const { id } = params;
+    const responseAPI = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    const recipe = await responseAPI.json();
+    this.setState({
+      recipe: recipe.meals[0],
+    });
+  }
+
+  async copyClipboard() {
+    try {
+      console.log('copy clipboard');
+      await navigator.clipboard.writeText(window.location.href);
+      console.log('copy clipboard write done');
+      this.setState({
+        copyClipboard: window.location.href,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  favoriteRecipe() {
+    const { isFavorite } = this.state;
+    this.setState({
+      isFavorite: !isFavorite,
+    });
+  }
+
+  renderIngredients() {
+    return [...Array(MAX_INGREDIENTS)]
+      .map((_, index) => this.renderIngredient(index));
+  }
+
+  renderIngredient(index) {
+    const { recipe } = this.state;
+    const ingredient = recipe[`strIngredient${index + 1}`];
+    const measure = recipe[`strMeasure${index + 1}`];
+    if (!ingredient || !measure) {
+      return;
+    }
+    const description = `${measure} of ${ingredient}`;
+    return (
+      <li data-testid={ `${index}-ingredient-step` }>
+        {description}
+      </li>
+    );
+  }
+
   render() {
+    const { recipe, copyClipboard, isFavorite } = this.state;
+    console.log(recipe);
     return (
       <div>
-        {/* <img src="" data-testid="recipe-photo" /> */}
-        <h1 data-testid="recipe-title"> Titulo </h1>
-        <button
-          type="button"
-          data-testid="share-btn"
-        >
-          Compartilhar
-        </button>
-        <button
-          type="button"
-          data-testid="favorite-btn"
-        >
-          Favoritar
-        </button>
+        <img
+          data-testid="recipe-photo"
+          src={ recipe.strMealThumb }
+          alt="imagem da receita"
+          width="200px"
+        />
+        <h1 data-testid="recipe-title">{recipe.strMeal}</h1>
+        <div>
+          <button
+            type="button"
+            data-testid="share-btn"
+            onClick={ this.copyClipboard }
+          >
+            <img src={ ShareIcon } alt="compartilhar" />
+          </button>
+        </div>
+        <span>
+          {copyClipboard ? 'Link copiado!' : ''}
+        </span>
+        <div>
+          <button
+            type="button"
+            data-testid="favorite-btn"
+            onClick={ () => this.favoriteRecipe() }
+            src={ !isFavorite ? whiteHeartIcon : blackHeartIcon }
+          >
+            <img src={ !isFavorite ? whiteHeartIcon : blackHeartIcon } alt="favorite" />
+          </button>
+        </div>
         <p data-testid="recipe-category"> Texto</p>
-        {/* <ul data-testid="${index}-ingredient-step">
-        <li>Ingredientes</li>
-      </ul> */}
-        <p data-testid="instructions"> Modo de preparo</p>
+        <p data-testid="recipe-category">{recipe.strCategory}</p>
+        <ul>{this.renderIngredients()}</ul>
+        <p data-testid="instructions">{recipe.strInstructions}</p>
 
         <button
           type="button"
@@ -35,3 +123,12 @@ class ProgressoComida extends React.Component {
 }
 
 export default ProgressoComida;
+
+ProgressoComida.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.string,
+  }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
