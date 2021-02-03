c8ff75e8afdc1dd8ca3de3a5fb0d38bc16ca18c3
Anderson Meira <contatodeanderson@gmail.com>
2021-02-03 17:11:03 -0300

    Add footer - refatorando componente

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,48 +3,32 @@ import { Link } from 'react-router-dom';
 import drinkIcon from '../../images/drinkIcon.svg';
 import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
-import './style.css';
+import StyledFooter from './styles';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer" data-testid="footer">
-        <Link to="/bebidas">
-          <button
-            type="button"
-            data-testid="drinks-bottom-btn"
-          >
-            <img
-              src={ drinkIcon }
-              alt="drinks"
-            />
-          </button>
-        </Link>
-        <Link to="/explorar">
-          <button
-            type="button"
-            data-testid="explore-bottom-btn"
-          >
-            <img
-              src={ exploreIcon }
-              alt="explore"
-            />
-          </button>
-        </Link>
-        <Link to="/comidas">
-          <button
-            type="button"
-            data-testid="food-bottom-btn"
-          >
-            <img
-              src={ mealIcon }
-              alt="meal"
-            />
-          </button>
-        </Link>
-      </footer>
-    );
-  }
+export default function Footer() {
+  return (
+    <StyledFooter data-testid="footer">
+      <Link to="/bebidas">
+        <img
+          src={ drinkIcon }
+          alt="drinks"
+          data-testid="drinks-bottom-btn"
+        />
+      </Link>
+      <Link to="/explorar">
+        <img
+          src={ exploreIcon }
+          alt="explore"
+          data-testid="explore-bottom-btn"
+        />
+      </Link>
+      <Link to="/comidas">
+        <img
+          src={ mealIcon }
+          alt="meal"
+          data-testid="food-bottom-btn"
+        />
+      </Link>
+    </StyledFooter>
+  );
 }
-
-export default Footer;
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  align-items: center;
+  background-color: rgb(229, 229, 229);
+  bottom: 0;
+  display: flex;
+  height: 60px;
+  justify-content: space-around;
+  position: fixed;
+  width: 100%;
+`;
+
+export default StyledFooter;
