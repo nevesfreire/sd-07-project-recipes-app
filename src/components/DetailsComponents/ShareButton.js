958a72c319f90156b1b6d18f666fa9a7b773ea83
Luise Rios <luisemiranda@gmail.com>
2021-02-04 14:36:28 -0300

    main group 10 - merge das branches + share btn

--- src/components/DetailsComponents/ShareButton.js
+++ src/components/DetailsComponents/ShareButton.js
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import copy from 'clipboard-copy';
 import { useHistory } from 'react-router-dom';
+import { Modal } from 'react-bootstrap';
 import shareIcon from '../../images/shareIcon.svg';
 
 function ShareButton() {
+  const [smShow, setSmShow] = useState(false);
+
   const history = useHistory();
   const path = history.location.pathname;
 
-  const handleShareBtn = ({ target }) => {
-    console.log(target);
-    target.addEventListener('click', () => {
-      copy(path);
-    });
-    alert('Link copiado!');
+  const copyLink = () => {
+    copy(`http://localhost:3000${path}`);
+  };
+
+  const handleShareBtn = () => {
+    copyLink();
+    setSmShow(true);
   };
 
   return (
-    <button
-      type="button"
-      data-testid="share-btn"
-      onClick={ (e) => handleShareBtn(e) }
-    >
-      <img
-        src={ shareIcon }
-        alt="Share Icon"
-      />
-    </button>
+    <div>
+      <button
+        type="button"
+        data-testid="share-btn"
+        onClick={ handleShareBtn }
+      >
+        <img
+          src={ shareIcon }
+          alt="Share Icon"
+        />
+      </button>
+      <Modal
+        size="sm"
+        show={ smShow }
+        onHide={ () => setSmShow(false) }
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">
+            Link copiado!
+          </Modal.Title>
+        </Modal.Header>
+      </Modal>
+    </div>
   );
 }
 
