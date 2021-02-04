2578a11cacdf1632b09f84f9585c76710833a71a
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-02-04 14:41:45 -0300

    inicio

--- src/pages/ComidasID/index.test.js
+++ src/pages/ComidasID/index.test.js
@@ -2,10 +2,16 @@ import React from 'react';
 // import { render, screen } from '@testing-library/react';
 import ComidasID from './index';
 import { renderWithRouterAndStore } from '../../test/testConfig';
+import storeMock from "../../test/mock";
 
-test('loads items eventually', async () => {
-  const { getByText } = renderWithRouterAndStore(<ComidasID />);
-  const element = getByText(/Ingredientes:/);
+test('rederiza pagina', async () => {
+  const { getByText, history } = renderWithRouterAndStore(
+    <ComidasID />,
+    '/comidas/53026',  
+    storeMock
+  );
+  history.push('/comidas/53026');
+  const element = getByText(/Pour the Galliano liqueur over ice. Fill the remainder of the glass with ginger ale and thats all there is to it. You now have a your very own GG./);
 
   // Wait for page to update with query text
   expect(element).toBeInTheDocument();
