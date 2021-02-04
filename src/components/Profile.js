2c4a863ba504cd029d0b2aa02ac98f4eaea9c7ee
CarolSi <a.carolg1@gmail.com>
2021-02-04 15:20:37 -0300

    bug-profile-teste-victor

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
 function Profile() {
-  const email = localStorage.getItem('user');
+  const email = Object.values(JSON.parse(localStorage.getItem('user')));
   const history = useHistory();
 
   const logOut = () => {
