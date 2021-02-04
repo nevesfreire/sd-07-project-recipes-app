ab3b87555dea1b1b56022ac339bc14c76b2a2ce2
dennerpl <denner.souzapl@gmail.com>
2021-02-03 21:03:39 -0300

    tela de receitas feitas faltando somente um requisito

---
+++ src/components/DoneRecipesCards.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import clipboardCopy from 'clipboard-copy';
+import shareIcon from '../images/shareIcon.svg';
+
+function DoneRecipesCards({
+  thumb,
+  title,
+  index,
+  id,
+  type,
+  tags,
+  category,
+  doneDate,
+  area,
+  alcoholicOrNot,
+}) {
+  const [copyMessage, setCopyMessage] = useState('');
+  const url = (text) => {
+    let returnText = '';
+    switch (text) {
+    case 'bebida':
+      returnText = 'bebidas';
+      break;
+    case 'comida':
+      returnText = 'comidas';
+      break;
+    default:
+      returnText = text;
+      break;
+    }
+    return returnText;
+  };
+
+  const copyClipboard = () => {
+    const urlarray = window.location.href.split('/');
+    const urlToCopy = `${urlarray[0]}//${urlarray[2]}/${type}/${id}`;
+    clipboardCopy(urlToCopy);
+    setCopyMessage('Link copiado!');
+  };
+
+  return (
+    <div>
+      <input
+        type="image"
+        data-testid={ `${index}-horizontal-share-btn` }
+        src={ shareIcon }
+        alt="compartilhar"
+        onClick={ copyClipboard }
+      />
+      {copyMessage}
+      <Link to={ `/${url(type)}/${id}` }>
+        <div>
+          <img
+            src={ thumb }
+            alt="Imagem do profile"
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <div>
+            Category:
+            {' '}
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              {`${area}${alcoholicOrNot} - ${category}`}
+            </p>
+          </div>
+          <div className="div" data-testid="page-div">
+            <p data-testid={ `${index}-horizontal-name` }>{title}</p>
+          </div>
+          <div>
+            Feito em:
+            {' '}
+            <p data-testid={ `${index}-horizontal-done-date` }>{doneDate}</p>
+          </div>
+          {tags.map((tag, counter) => (
+            <p key={ counter } data-testid={ `${index}-${tag}-horizontal-tag` }>
+              {tag}
+            </p>
+          ))}
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+DoneRecipesCards.propTypes = {
+  title: PropTypes.string.isRequired,
+  thumb: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  doneDate: PropTypes.string.isRequired,
+  area: PropTypes.string.isRequired,
+  alcoholicOrNot: PropTypes.string.isRequired,
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default DoneRecipesCards;
--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,9 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HeaderNoSearch from '../components/HeaderNoSearch';
+import DoneRecipesCards from '../components/DoneRecipesCards';
+import * as localStorageFunctions from '../services/localStorageFunctions';
 
 function DoneRecipes() {
+  const [initialDoneRecipes, setInitialDoneRecipes] = useState([]);
+  const [atualDoneRecipes, setAtualDoneRecipes] = useState([]);
+
+  useEffect(() => {
+    setInitialDoneRecipes(localStorageFunctions.getDoneRecipesLocalStorage());
+  }, []);
+
+  useEffect(() => {
+    setAtualDoneRecipes(initialDoneRecipes);
+  }, [initialDoneRecipes]);
+
+  const filterByFood = () => {
+    const arrayOfFood = atualDoneRecipes.filter((item) => item.type === 'comida');
+    setAtualDoneRecipes(arrayOfFood);
+  };
+
+  const filterByDrink = () => {
+    const arrayOfDrink = atualDoneRecipes.filter((item) => item.type === 'bebida');
+    setAtualDoneRecipes(arrayOfDrink);
+  };
+
+  const resetArrayToRender = () => {
+    setAtualDoneRecipes(initialDoneRecipes);
+  };
+
+  const renderElements = (array) => {
+    const initialarray = [...array];
+    const finalArray = initialarray
+      .map((foodOrDrink, index) => (
+        <DoneRecipesCards
+          title={ foodOrDrink.name }
+          key={ index }
+          index={ index }
+          id={ foodOrDrink.id }
+          thumb={ foodOrDrink.image }
+          type={ foodOrDrink.type }
+          tags={ foodOrDrink.tags }
+          category={ foodOrDrink.category }
+          doneDate={ foodOrDrink.doneDate }
+          area={ foodOrDrink.area }
+          alcoholicOrNot={ foodOrDrink.alcoholicOrNot }
+        />
+      ));
+    return finalArray;
+  };
   return (
-    <HeaderNoSearch title="Receitas Feitas" />
+    <div>
+      <HeaderNoSearch title="Receitas Feitas" />
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ resetArrayToRender }
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+        onClick={ filterByFood }
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ filterByDrink }
+      >
+        Drinks
+      </button>
+      {atualDoneRecipes === undefined ? (
+        <p>Loading</p>
+      ) : (
+        renderElements(atualDoneRecipes)
+      )}
+    </div>
   );
 }
 
