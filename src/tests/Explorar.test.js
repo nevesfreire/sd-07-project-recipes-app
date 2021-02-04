1f0f37433ef9cd632566892bad7f242a7ef5c2be
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-02-04 11:03:14 -0300

    main-group-13-requisito-test-explorar-removendo erros de lint

--- src/tests/Explorar.test.js
+++ src/tests/Explorar.test.js
@@ -1,62 +1,60 @@
 import React from 'react';
 import { screen, fireEvent } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
-import Comidas from '../pages/Comidas'
+import Comidas from '../pages/Comidas';
 import Explorar from '../pages/Explorar';
 
-
-
 describe('Testa o comportamente do componente TelaExplorar.js', () => {
-    it('Verifica a mudança de rota para \'explorar\'', async () => {
-        const { history } = renderWithRouter(<Comidas />);
-        const { pathname } = history.location;
-        const explorarIcon = await screen.findByTestId('explore-bottom-btn');
+  it('Verifica a mudança de rota para \'explorar\'', async () => {
+    const { history } = renderWithRouter(<Comidas />);
+    const { pathname } = history.location;
+    const explorarIcon = await screen.findByTestId('explore-bottom-btn');
 
-        expect(explorarIcon).toBeInTheDocument();
+    expect(explorarIcon).toBeInTheDocument();
 
-        fireEvent.click(explorarIcon);
-        expect(pathname).toBe('/explorar');
-    });
-    
-    it('Verifica os elementos na tela', async() => {
-        renderWithRouter(<Explorar />);
-        const exploreFoods = await screen.findByTestId('explore-food');
-        const exploreDrinks = await screen.findByTestId('explore-drinks');
-        const profileIcon = screen.getByTestId('profile-top-btn');
-        const pageTitle = screen.getByTestId('page-title');
+    fireEvent.click(explorarIcon);
+    expect(pathname).toBe('/explorar');
+  });
 
-        expect(profileIcon).toBeInTheDocument();
-        expect(pageTitle).toBeInTheDocument();
-        expect(exploreFoods).toBeInTheDocument();
-        expect(exploreDrinks).toBeInTheDocument();
+  it('Verifica os elementos na tela', async () => {
+    renderWithRouter(<Explorar />);
+    const exploreFoods = await screen.findByTestId('explore-food');
+    const exploreDrinks = await screen.findByTestId('explore-drinks');
+    const profileIcon = screen.getByTestId('profile-top-btn');
+    const pageTitle = screen.getByTestId('page-title');
 
-        expect(profileIcon.tagName).toBe('A');
-        expect(pageTitle.textContent).toBe('Explorar');
-        expect(exploreFoods.textContent).toBe('Explorar Comidas');
-        expect(exploreDrinks.textContent).toBe('Explorar Bebidas');
+    expect(profileIcon).toBeInTheDocument();
+    expect(pageTitle).toBeInTheDocument();
+    expect(exploreFoods).toBeInTheDocument();
+    expect(exploreDrinks).toBeInTheDocument();
 
-        const drinkIcon = await screen.findByTestId('drinks-bottom-btn');
-        const foodIcon = await screen.findByTestId('food-bottom-btn');
+    expect(profileIcon.tagName).toBe('A');
+    expect(pageTitle.textContent).toBe('Explorar');
+    expect(exploreFoods.textContent).toBe('Explorar Comidas');
+    expect(exploreDrinks.textContent).toBe('Explorar Bebidas');
 
-        expect(drinkIcon.tagName).toBe('A');
-        expect(foodIcon.tagName).toBe('A');
-    });
-    it('Verifica a mudança de rota para \'/explorar/comidas\'', async() => {
-        const { history } = renderWithRouter(<Explorar />);
-        const { pathname } = history.location;
-        const exploreFoods = await screen.findByTestId('explore-food');
-        
-        fireEvent.click(exploreFoods);
+    const drinkIcon = await screen.findByTestId('drinks-bottom-btn');
+    const foodIcon = await screen.findByTestId('food-bottom-btn');
 
-        expect(pathname).toBe('/explorar/comidas');
-    });
-    it('Verifica a mudança de rota para \'/explorar/bebidas\'', async() => {
-        const { history } = renderWithRouter(<Explorar />);
-        const { pathname } = history.location;
-        const exploreDrinks = await screen.findByTestId('explore-drinks');
-        
-        fireEvent.click(exploreDrinks);
+    expect(drinkIcon.tagName).toBe('A');
+    expect(foodIcon.tagName).toBe('A');
+  });
+  it('Verifica a mudança de rota para \'/explorar/comidas\'', async () => {
+    const { history } = renderWithRouter(<Explorar />);
+    const { pathname } = history.location;
+    const exploreFoods = await screen.findByTestId('explore-food');
 
-        expect(pathname).toBe('/explorar/bebidas');
-    });
+    fireEvent.click(exploreFoods);
+
+    expect(pathname).toBe('/explorar/comidas');
+  });
+  it('Verifica a mudança de rota para \'/explorar/bebidas\'', async () => {
+    const { history } = renderWithRouter(<Explorar />);
+    const { pathname } = history.location;
+    const exploreDrinks = await screen.findByTestId('explore-drinks');
+
+    fireEvent.click(exploreDrinks);
+
+    expect(pathname).toBe('/explorar/bebidas');
+  });
 });
