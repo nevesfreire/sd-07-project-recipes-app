f832f384c6efa7a93d1492559387ac8da6af38a0
cezar88augusto <cezar88augusto@gmail.com>
2021-02-04 13:40:32 -0300

    Requisitos 82 83 84 85 86 87

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import { getItem, initialize } from '../../services/localStorage';
 
 function Profile({ history, search = false }) {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    const emailFromLocalStorage = getItem('user');
+    if (!emailFromLocalStorage) initialize();
+    else setEmail(emailFromLocalStorage.email);
+  }, []);
+
+  const localStorageClear = () => {
+    localStorage.clear();
+  };
+
   return (
     <>
       <Header history={ history } search={ search } />
-      <p>Profile</p>
+      <p data-testid="profile-email">{ email }</p>
+      <ul>
+        <li>
+          <Link to="receitas-feitas">
+            <button
+              type="button"
+              data-testid="profile-done-btn"
+            >
+              Receitas Feitas
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link to="receitas-favoritas">
+            <button
+              type="button"
+              data-testid="profile-favorite-btn"
+            >
+              Receitas Favoritas
+            </button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <button
+              type="button"
+              data-testid="profile-logout-btn"
+              onClick={ () => localStorageClear() }
+            >
+              Sair
+            </button>
+          </Link>
+        </li>
+      </ul>
       <Footer />
     </>
   );
