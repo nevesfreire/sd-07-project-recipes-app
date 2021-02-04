96092390953bf7657f01189b7a1c95f96c6e6edf
Luiz Mariz <lfsmariz@gmail.com>
2021-02-04 12:26:02 -0300

    all requirements

--- src/Components/DetailScreen/index.jsx
+++ src/Components/DetailScreen/index.jsx
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { fetchAllCocktails, singleCocktail } from '../../API/apiCocktails';
 import { fetchAllRecipes, singleMeal } from '../../API/apiMeals';
+import {
+  checkComplete,
+  checkFavorite,
+  checkProgress,
+} from '../../helper/checkHelper';
+import useCopyToClipboard from '../../Hooks/useCopyToClipboard';
 import RecomendationCard from '../RecomedationCard';
+import blackHeartIcon from '../../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../../images/whiteHeartIcon.svg';
 
 const DetailScreen = (props) => {
+  const TIME = 5000;
   const { page, id } = props;
   const [recipe, setRecipe] = useState({});
   const [ingredients, setIngredients] = useState([]);
   const [measure, setMeasure] = useState([]);
   const [recommendation, setRecomendation] = useState([]);
+  const [copied, setCopied] = useCopyToClipboard(TIME);
+  const [favorite, setFavorite] = useState(false);
 
   const resolveFetch = (resolve) => {
     setRecipe(resolve);
@@ -26,27 +39,74 @@ const DetailScreen = (props) => {
     }
   }, [page, id]);
 
+  useEffect(() => {
+    setFavorite(checkFavorite(id));
+  }, [id]);
+
   const recommendationMount = (arrRecommendation) => {
+    const arrTemp = arrRecommendation || [];
     const maxSize = 6;
     const minSize = 0;
     const keyObject = page === 'comidas' ? 'Drink' : 'Meal';
-    if (arrRecommendation.length === minSize) {
-      return null;
-    }
+    const pageRec = page === 'comidas' ? 'bebidas' : 'comidas';
     return (
       <>
-        {arrRecommendation.slice(minSize, maxSize).map((elem, index) => (
+        {arrTemp.slice(minSize, maxSize).map((elem, index) => (
           <RecomendationCard
             key={ elem[`str${keyObject}`] }
             thumb={ elem[`str${keyObject}Thumb`] }
             foodName={ elem[`str${keyObject}`] }
             index={ index }
-            page={ page }
+            page={ pageRec }
             idFood={ elem[`id${keyObject}`] }
           />
         ))}
       </>
     );
+  };
+
+  const startRecipe = () => {
+    if (checkComplete(id)) {
+      return null;
+    }
+    return (
+      <Link to={ `/${page}/${id}/in-progress` }>
+        <button
+          className="btn-start"
+          data-testid="start-recipe-btn"
+          type="button"
+        >
+          {checkProgress(id, page)}
+        </button>
+      </Link>
+    );
+  };
+
+  const favoriteIcon = (fav) => {
+    const favReturn = fav ? (
+      <img data-testid="favorite-btn" src={ blackHeartIcon } alt="blackHeart" />
+    ) : (
+      <img data-testid="favorite-btn" src={ whiteHeartIcon } alt="whiteHeart" />
+    );
+    return favReturn;
+  };
+
+  const favoriteBtn = () => {
+    const objRecipe = {
+      id,
+      type: recipe.typeRecipe,
+      area: recipe.areaRecipe,
+      category: recipe.categoryRecipe,
+      alcoholicOrNot: recipe.alcoholic,
+      name: recipe.nameRecipe,
+      image: recipe.imgRecipe,
+    };
+    const stringFavorite = localStorage.getItem('favoriteRecipes');
+    const jsonFavorite = stringFavorite !== null ? JSON.parse(stringFavorite) : [];
+    const allValue = [...jsonFavorite, objRecipe];
+    const stringRecipe = JSON.stringify(allValue);
+    localStorage.setItem('favoriteRecipes', stringRecipe);
+    setFavorite(!favorite);
   };
 
   return (
@@ -58,11 +118,15 @@ const DetailScreen = (props) => {
         data-testid="recipe-photo"
       />
       <p data-testid="recipe-title">{recipe.nameRecipe}</p>
-      <button type="button" data-testid="share-btn">
-        Compartilhar
+      <button
+        type="button"
+        data-testid="share-btn"
+        onClick={ () => setCopied(`/${page}/${id}`) }
+      >
+        {copied ? 'Link copiado!' : 'Compartilhar'}
       </button>
-      <button type="button" data-testid="favorite-btn">
-        Favoritar
+      <button type="button" onClick={ favoriteBtn }>
+        {favoriteIcon(favorite)}
       </button>
       <p data-testid="recipe-category">
         {`${recipe.categoryRecipe} - ${recipe.alcoholic}`}
@@ -81,15 +145,14 @@ const DetailScreen = (props) => {
       <div className="container-cards">
         {recommendationMount(recommendation)}
       </div>
-      <button
-        className="btn-start"
-        data-testid="start-recipe-btn"
-        type="button"
-      >
-        Iniciar Receita
-      </button>
+      {startRecipe()}
     </div>
   );
 };
 
 export default DetailScreen;
+
+DetailScreen.propTypes = {
+  page: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
