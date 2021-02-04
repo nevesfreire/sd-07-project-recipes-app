7da826baeb31c2f5c13ddba1bc10a692d35cec1d
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-02-04 18:29:14 -0300

    comecando requisito 54

--- src/pages/DoneRecipes/index.jsx
+++ src/pages/DoneRecipes/index.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Header } from '../../components';
+import { RecipesContext } from '../../context';
 
 export default function DoneRecipes() {
+  const { doneRecipes, setDoneRecipes } = useContext(RecipesContext);
+  const defaultRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+
   return (
-    <Header title="Receitas Feitas" />
+    <div>
+      <Header title="Receitas Feitas" />
+      <main>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => setDoneRecipes(defaultRecipes) }
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-food-btn"
+          onClick={ () => {
+            setDoneRecipes(
+              defaultRecipes.filter((recipe) => recipe.type === 'comida'),
+            );
+          } }
+        >
+          Food
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => {
+            setDoneRecipes(
+              defaultRecipes.filter((recipe) => recipe.type === 'bebida'),
+            );
+          } }
+        >
+          Drink
+        </button>
+        {
+
+        }
+      </main>
+    </div>
   );
 }
