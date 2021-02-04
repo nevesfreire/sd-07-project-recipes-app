c274f1d68689cddf06c5ae9823ac92a5e5234df2
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2021-02-04 16:42:36 -0300

    59 pronto, 55 pronto, seria se dar com os teste id

--- src/components/CardMadeRecipes.js
+++ src/components/CardMadeRecipes.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import shareIcon from '../images/shareIcon.svg';
 
-function CardMadeRecipes(doneRecipe, { index }) {
-  console.log(doneRecipe, 'recipe');
+function CardMadeRecipes({ recipe, index }) {
   const [clipy, setClipy] = useState(false);
 
   // const url = document.URL;
@@ -12,14 +11,19 @@ function CardMadeRecipes(doneRecipe, { index }) {
 
   return (
     <div data-testid={ `${index}-horizontal-name` }>
-      <div>{doneRecipe.recipe.area}</div>
-      <div>{doneRecipe.recipe.category}</div>
-      <div data-testid="0-horizontal-done-date">{doneRecipe.recipe.doneDate}</div>
-      {/* <div>{doneRecipe.tags[0] && doneRecipe.tags[1]}</div> */}
+      <Link to={ `/comidas/${recipe.id}` }>
+        <div>{recipe.name}</div>
+      </Link>
+      <div>{recipe.area}</div>
+      <div>{recipe.category}</div>
+      <div data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</div>
+      <div>{recipe.tags[0,1]}</div>
+      <Link to={ `/comidas/${recipe.id}` }>
 
-      <img data-testid={`${index}-horizontal-image`} alt="recipeImg" src={ doneRecipe.recipe.image } />
+        <img data-testid="0-horizontal-image" alt="recipeImg" src={ recipe.image } />
+      </Link>
 
-      <button type="button" onClick={ () => {} }>
+      <button data-testid={ `${index}-horizontal-share-btn` } type="button" onClick={ () => {} }>
         <img src={ shareIcon } alt="img " />
       </button>
     </div>
