1915664bda10c1836c1f34cd3384d72dfce9e90f
Filipi Firmino <contatoic3web@gmail.com>
2021-02-04 19:21:35 -0300

    Animation libe in login

--- src/Components/LoginForm/index.jsx
+++ src/Components/LoginForm/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { Animated } from 'react-animated-css';
 import { useHistory } from 'react-router';
 import RecipeContext from '../../Context/RecipeContext';
 import { verifyLogin, saveTokens } from '../../helper/loginHelper';
@@ -23,45 +24,58 @@ const LoginForm = () => {
   return (
     <div className="container-form-login">
       <div className="container-login-int">
-        <form action="" className="form-login">
-          <h1 className="title-login">Bem-vindo</h1>
-          <input
-            type="email"
-            name="userEmail"
-            value={ userEmail }
-            data-testid="email-input"
-            onChange={ ({ target }) => setEmail(target.value) }
-            className="input"
-            placeholder="exemplo@exemplo.com"
-          />
+        <Animated
+          animationIn="bounceInLeft"
+          isVisible
+          animationInDuration="2000"
+          className="form-login"
+        >
+          <form action="" className="form-login">
+            <h1 className="title-login">Bem-vindo</h1>
+            <input
+              type="email"
+              name="userEmail"
+              value={ userEmail }
+              data-testid="email-input"
+              onChange={ ({ target }) => setEmail(target.value) }
+              className="input"
+              placeholder="exemplo@exemplo.com"
+            />
 
-          <input
-            type="password"
-            name="userPass"
-            value={ userPass }
-            data-testid="password-input"
-            onChange={ ({ target }) => setPass(target.value) }
-            className="input"
-            placeholder="senha"
-          />
+            <input
+              type="password"
+              name="userPass"
+              value={ userPass }
+              data-testid="password-input"
+              onChange={ ({ target }) => setPass(target.value) }
+              className="input"
+              placeholder="senha"
+            />
 
-          <button
-            className="btn-login"
-            type="button"
-            data-testid="login-submit-btn"
-            disabled={ statusButton }
-            onClick={ () => {
-              saveTokens();
-              dispatch({ type: 'SET_USER', userEmail });
-              history.push('/comidas');
-            } }
-          >
-            Entrar
-          </button>
-        </form>
+            <button
+              className="btn-login"
+              type="button"
+              data-testid="login-submit-btn"
+              disabled={ statusButton }
+              onClick={ () => {
+                saveTokens();
+                dispatch({ type: 'SET_USER', userEmail });
+                history.push('/comidas');
+              } }
+            >
+              Entrar
+            </button>
+          </form>
+        </Animated>
       </div>
       <div className="image-login">
-        <img src={ ImageBack } alt="login-side" />
+        <Animated
+          animationIn="bounceInRight"
+          isVisible
+          animationInDuration="2000"
+        >
+          <img src={ ImageBack } alt="login-side" />
+        </Animated>
       </div>
     </div>
   );
