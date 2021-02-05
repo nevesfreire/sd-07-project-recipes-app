8c6db7c5e14be8c14f29774b0114b6a1c3691825
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-02-05 02:29:13 -0300

    Removi o carrossel

--- src/components/Recomendations/index.js
+++ src/components/Recomendations/index.js
@@ -1,75 +1,73 @@
-// import React, { useEffect, useState } from 'react';
-// import PropTypes from 'prop-types';
-// import Carousel from 'react-elastic-carousel';
-// import Item from './Item';
-// import './styles.css';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import Item from './Item';
+import './style.css';
 
-// const breakPoints = [
-//   { width: 1, itemsToShow: 2 },
-//   { width: 550, itemsToShow: 2 },
-//   { width: 768, itemsToShow: 2 },
-//   { width: 1200, itemsToShow: 2 },
-// ];
+const Recomendation = ({ title }) => {
+  const [recomendations, setRecomendations] = useState([]);
+  console.log(recomendations);
+  useEffect(() => {
+    async function handleRecomendation() {
+      if (title.includes('comidas')) {
+        const request = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+        const response = await request.json();
+        return setRecomendations(response.drinks);
+      }
+      const request = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const response = await request.json();
+      return setRecomendations(response.meals);
+    }
+    handleRecomendation();
+  });
+  const SEIS = 6;
+  return (
+    <div>
+      <h2>{`Recomendations - ${title}`}</h2>
+      <div className="carousel">
+        {title.includes('comidas') ? (
+          <div className="carousel-card">
+            {recomendations
+              .filter((_, index) => index < SEIS)
+              .map(({ strDrink, strDrinkThumb }, index) => (
+                <div key={ strDrink }>
+                  <Item className="cards">
+                    <img
+                      src={ strDrinkThumb }
+                      alt={ strDrink }
+                      data-testid={ `${index}-recomendation-card` }
+                    />
+                  </Item>
+                  <h4 data-testid={ `${index}-recomendation-title` }>
+                    {strDrink}
+                  </h4>
+                </div>
+              ))}
+          </div>
+        ) : (
+          <div className="carousel-card">
+            {recomendations
+              .filter((_, index) => index < SEIS)
+              .map(({ strMeal, strMealThumb }, index) => (
+                <div key={ strMeal }>
+                  <Item className="cards">
+                    <img
+                      src={ strMealThumb }
+                      alt={ strMeal }
+                      data-testid={ `${index}-recomendation-card` }
+                    />
+                  </Item>
+                  <h4 data-testid={ `${index}-recomendation-title` }>
+                    {strMeal}
+                  </h4>
+                </div>
+              ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-// const Recomendation = ({ title }) => {
-//   const [recomendations, setRecomendations] = useState([]);
-//   console.log(recomendations);
-//   useEffect(() => {
-//     async function handleRecomendation() {
-//       if (title.includes('comidas')) {
-//         const request = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
-//         const response = await request.json();
-//         return setRecomendations(response.drinks);
-//       }
-//       const request = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-//       const response = await request.json();
-//       return setRecomendations(response.meals);
-//     }
-//     handleRecomendation();
-//   }, []);
-//   const SEIS = 6;
-//   return (
-//     <div>
-//       <h2>{`Recomendations - ${title}`}</h2>
-//       <div className="carousel">
-//         {title.includes('comidas') ? (
-//           <Carousel breakPoints={ breakPoints }>
-//             {recomendations
-//               .filter((_, index) => index < SEIS)
-//               .map(({ strDrink, strDrinkThumb }, index) => (
-//                 <Item
-//                   key={ strDrink }
-//                   data-testid={ `${index}-recomendation-card` }
-//                 >
-//                   <img src={ strDrinkThumb } alt={ strDrink } />
-//                   <h2 data-testid={ `${index}-recomendation-title` }>
-//                     {strDrink}
-//                   </h2>
-//                 </Item>
-//               ))}
-//           </Carousel>
-//         ) : (
-//           <Carousel breakPoints={ breakPoints }>
-//             {recomendations
-//               .filter((_, index) => index < SEIS)
-//               .map(({ strMeal, strMealThumb }, index) => (
-//                 <Item
-//                   key={ strMeal }
-//                   data-testid={ `${index}-recomendation-card` }
-//                 >
-//                   <img src={ strMealThumb } alt={ strMeal } />
-//                   <h2 data-testid={ `${index}-recomendation-title` }>
-//                     {strMeal}
-//                   </h2>
-//                 </Item>
-//               ))}
-//           </Carousel>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
+Recomendation.propTypes = { title: PropTypes.string.isRequired };
 
-// Recomendation.propTypes = { title: PropTypes.string.isRequired };
-
-// export default Recomendation;
+export default Recomendation;
