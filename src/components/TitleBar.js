a15f2b1fdd6b51c5d53c277518d62a6d1422be7c
Anderson Santana <anderslash@gmail.com>
2021-02-05 13:17:10 -0300

    Importacao de Proptypes em TitleBar

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
