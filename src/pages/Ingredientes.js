7626a9cc066154c160840acc7e8c6f3cf1949441
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-02-04 23:53:57 -0300

    Resolvendo lint

--- src/pages/Ingredientes.js
+++ src/pages/Ingredientes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { sendDrinkRecipes, sendMealRecipes } from '../redux/actions';
 import { apiTheMealDB, apiTheCocktailDB } from '../services';
