77ee345c9cf9fa4c1553bca9e544e6b37e2205bb
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2021-02-04 22:57:59 -0300

    Componente Compartilhar

---
+++ src/components/ShareButton/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import copy from 'clipboard-copy';
+import { shareIcon } from '../../images';
+
+import StyledImage from './styles';
+
+export default function ShareButton(props) {
+  const [propsState] = useState(props);
+  const { recipeId, type } = propsState;
+
+  const handleClick = () => {
+    copy(`${window.location.origin}/${type}/${recipeId}`);
+  };
+
+  return (
+    <StyledImage
+      src={ shareIcon }
+      onClick={ handleClick }
+      alt="Share"
+    />
+  );
+}
---
+++ src/components/ShareButton/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Image } from 'react-bootstrap';
+
+const StyledImage = styled(Image)`
+  width: 26px;
+  height: 26px;
+`;
+
+export default StyledImage;
