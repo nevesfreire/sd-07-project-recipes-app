f3059f2a5e10b0db5016a45974eb0a0bc84a5cd9
Lets <letsferreiratx@gmail.com>
2021-02-05 18:36:47 -0300

    tela de receitas em progresso ok

--- src/pages/RecipesInProgress/index.js
+++ src/pages/RecipesInProgress/index.js
@@ -7,12 +7,9 @@ import './style.css';
 
 function RecipesInProgress() {
   const { category, idReceita } = useParams();
-  const inProgress = {
-    cocktails: { [idReceita]: [] },
-    meals: { [idReceita]: [] },
-  };
-  const [progress, setProgress] = useState(inProgress);
+  const [progress, setProgress] = useState([]);
   const [details, setDetails] = useState();
+  const [disabled, setDisabled] = useState(true);
   useEffect(() => {
     if (category === 'comidas') {
       RequestData(
@@ -28,42 +25,6 @@ function RecipesInProgress() {
       });
     }
   }, [category, idReceita]);
-
-  useEffect(() => {
-    const localProgress = getStorage('inProgressRecipes');
-    if (!localProgress) {
-      const inProgress = {
-        cocktails: { [idReceita]: [] },
-        meals: { [idReceita]: [] },
-      };
-      setStorage('inProgressRecipes', inProgress);
-    }
-    else if (localProgress) {
-      const newprogress = { localProgress, ...progress };
-      console.log(localProgress);
-      setStorage('inProgressRecipes', newprogress);
-    }
-  }, [progress, idReceita]);
-
-  function handleProgress({ target }) {
-    const { name, value } = target;
-    const localProgress = getStorage('inProgressRecipes');
-
-    if (category === 'comidas') {
-      setProgress({
-        ...localProgress,
-        ...progress,
-        meals: { [idReceita]: [...progress.meals[idReceita], value] },
-      });
-    } else {
-      setProgress({
-        ...localProgress,
-        ...progress,
-        cocktails: { [idReceita]: [...progress.cocktails[idReceita], value] },
-      });
-    }
-
-  }
 
   function takeIngredients() {
     if (!details) return undefined;
@@ -83,38 +44,105 @@ function RecipesInProgress() {
     );
   }
 
+  // function handleProgress({ target }) {
+  //   const { name, value } = target;
+  //   const localProgress = getStorage('inProgressRecipes');
+
+  //   if (category === 'comidas') {
+  //     setProgress({
+  //       ...localProgress,
+  //       ...progress,
+  //       meals: { [idReceita]: [...progress.meals[idReceita], value] },
+  //     });
+  //   } else {
+  //     setProgress({
+  //       ...localProgress,
+  //       ...progress,
+  //       cocktails: { [idReceita]: [...progress.cocktails[idReceita], value] },
+  //     });
+  //   }
+  // }
+
+  function handleProgress({ target }) {
+    const { name, value, checked } = target;
+    console.log(name, value, checked);
+    let array = [];
+    if (checked) {
+      array = [...progress, value];
+    } else {
+      array = progress.filter((item) => item !== value);
+    }
+    if (array.length === takeIngredients().length) setDisabled(false);
+    const localProgress = getStorage('inProgressRecipes');
+    const recipeType = category === 'comidas' ? 'meals' : 'cocktails';
+
+    setStorage('inProgressRecipes', {
+      ...localProgress,
+      [recipeType]: { [idReceita]: [...array] },
+    });
+    setProgress(array);
+  }
+
+  useEffect(() => {
+    let localProgress = getStorage('inProgressRecipes');
+    if (!localProgress) {
+      setStorage('inProgressRecipes', { meals: {}, cocktails: {} });
+      localProgress = { meals: {}, cocktails: {} };
+    }
+    let currRecipe = [];
+    if (category === 'comidas') {
+      currRecipe = Object.entries(localProgress.meals).filter(
+        (item) => item[0] === idReceita,
+      );
+    } else {
+      currRecipe = Object.entries(localProgress.cocktails).filter(
+        (item) => item[0] === idReceita,
+      );
+    }
+    const zero = 0;
+    if (currRecipe.length !== zero) setProgress(currRecipe[0][1]);
+  }, []);
+
   return (
     <div>
       {!details && 'Loading...'}
-      {details && (
-        <DetailsHeader details={ details } />)}
+      {details && <DetailsHeader details={ details } />}
       <h2>Ingredients</h2>
-      {details && takeIngredients().map((ingredients, index) => (
-        <div key={ index } data-testid={ `${index}-ingredient-step` }>
-          <input
-            onClick={ handleProgress }
-            type="checkbox"
-            id={ index }
-            required
-            name={ index }
-            value={ details[ingredients] }
-          />
-          <label className="strikethrough" htmlFor={ index }>
-            {' '}
-            {`${details[ingredients]} - ${
-              details[takeMeasure()[index]] || ''
-            }`}
-          </label>
-          <br />
-        </div>
-      ))}
+      {details
+        && takeIngredients().map((ingredients, index) => (
+          <div key={ index } data-testid={ `${index}-ingredient-step` }>
+            <input
+              onClick={ handleProgress }
+              type="checkbox"
+              id={ index }
+              required
+              name={ index }
+              checked={
+                progress
+                  ? progress.find((item) => item === details[ingredients])
+                  : false
+              }
+              value={ details[ingredients] }
+            />
+            <label className="strikethrough" htmlFor={ index }>
+              {' '}
+              {`${details[ingredients]} - ${
+                details[takeMeasure()[index]] || ''
+              }`}
+            </label>
+            <br />
+          </div>
+        ))}
 
       <h2>Instructions</h2>
-      {details && (
-        <p data-testid="instructions">{details.strInstructions}</p>)}
+      {details && <p data-testid="instructions">{details.strInstructions}</p>}
 
       <Link to="/receitas-feitas">
-        <button type="submit" data-testid="finish-recipe-btn">
+        <button
+          disabled={ disabled }
+          type="submit"
+          data-testid="finish-recipe-btn"
+        >
           Finalizar Receita
         </button>
       </Link>
