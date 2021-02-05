5a7e20f53b766d659be3c9a26329956dbf28084c
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-02-05 16:10:37 -0300

    lint

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,8 @@ import RecipeContext from '../Context/Context';
 
 function Card({ name, thumb, index, id, type }) {
   const { setTypeAndIdDetails } = useContext(RecipeContext);
-  console.log('chegou no Card')
   return (
     <Link to={ `/${type}/${id}` }>
-      {console.log('chegou no return do Card. Name:', name)}
       <div
         className="recipe-card"
         onKeyDown=""
