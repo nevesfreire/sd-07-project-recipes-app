45231c80a9991e45caa4fad54ac8c61c1fe1db6c
brendalima <brenda.c.lima@poli.ufrj.br>
2021-02-06 10:57:05 -0300

    modificando

--- src/pages/CocktailDetails.js
+++ src/pages/CocktailDetails.js
@@ -20,6 +20,7 @@ function CocktailDetails() {
   const [mealsRecommendations, setMealsRecommendations] = useState([]);
 
   useEffect(() => {
+    
     setRecipeId(params.id);
     getMealsRecommendations()
       .then((res) => setMealsRecommendations(res));
