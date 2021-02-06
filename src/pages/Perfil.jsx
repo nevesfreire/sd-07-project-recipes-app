46bd021123a953f3c1295d94ecca8cb25d630298
lucasvr39 <lucasveras1994@gmail.com>
2021-02-06 17:53:23 -0300

    resolve erros de lint

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -4,44 +4,43 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Perfil() {
-
   const handleLocalStorageOnLogout = () => {
-    if(localStorage) localStorage.clear();
+    if (localStorage) localStorage.clear();
   };
 
   const userEmail = JSON.parse(localStorage.user).email;
-  
+
   return (
     <div>
       <Header name="Perfil" button={ false } />
-        <h4 data-testid="profile-email">
-          {userEmail}
-        </h4>
-        <Link to="/receitas-feitas">
+      <h4 data-testid="profile-email">
+        {userEmail}
+      </h4>
+      <Link to="/receitas-feitas">
         <button
-         data-testid="profile-done-btn"
-         type="button"
+          data-testid="profile-done-btn"
+          type="button"
         >
           Receitas Feitas
         </button>
-        </Link>
-        <Link to="/receitas-favoritas">
-          <button
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button
           data-testid="profile-favorite-btn"
           type="button"
-          >
-            Receitas Favoritas
-          </button>
-        </Link>
-        <Link to="/">
-          <button
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
           data-testid="profile-logout-btn"
           type="button"
           onClick={ handleLocalStorageOnLogout }
-          >
-            Sair
-          </button>
-        </Link>
+        >
+          Sair
+        </button>
+      </Link>
       <Footer />
     </div>
   );
