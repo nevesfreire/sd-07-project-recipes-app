318a7229caab33f18775ad0dc132395906e6d857
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-02-06 18:47:19 -0300

    feat: recomendations carousel effect.

--- src/Components/RecomendationCards.js
+++ src/Components/RecomendationCards.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Carousel, Button } from 'react-bootstrap';
 // import Card from './Card';
 import RecipesContext from '../context/RecipesContext';
 
@@ -35,47 +36,112 @@ function RecomendationCards(props) {
 
   if (!isFetching) {
     return (
-      <div>
-        {recomendations.map((recomendation, index) => {
-          if (type === 'bebida') {
-            return (
-              <div key={ index } data-testid={ `${index}-recomendation-card` }>
-                <div>
-                  <Link to={ `/comidas/${recomendation.idMeal}` }>
-                    <img
-                      src={ recomendation.strMealThumb }
-                      data-testid={ `${index}-card-img` }
-                      alt="Meal"
-                    />
-                    <h2
-                      data-testid={ `${index}-card-name` }
-                    >
-                      {recomendation.strMeal}
-                    </h2>
-                  </Link>
-                </div>
-              </div>
-            );
-          }
-          return (
-            <div key={ index } data-testid={ `${index}-recomendation-card` }>
-              <div>
-                <Link to={ `/bebidas/${recomendation.idDrink}` }>
-                  <img
-                    src={ recomendation.strDrinkThumb }
-                    data-testid={ `${index}-card-img` }
-                    alt="Drink"
-                  />
-                  <h2
-                    data-testid={ `${index}-card-name` }
+      <div
+        style={ {
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center' } }
+      >
+        <h2>Recomendações:</h2>
+        <Carousel>
+          {recomendations.map((recomendation, index, array) => {
+            const nextIndex = () => {
+              if (index === array.length - 1) return zero;
+              return index + 1;
+            };
+            if (type === 'bebida') {
+              return (
+                <Carousel.Item>
+                  <div key={ index } data-testid={ `${index}-recomendation-card` }>
+                    <div>
+                      <Link to={ `/comidas/${recomendation.idMeal}` }>
+                        <img
+                          src={ recomendation.strMealThumb }
+                          data-testid={ `${index}-card-img` }
+                          alt="Meal"
+                          style={ { margin: '15px', width: '50%', borderRadius: '15px' } }
+                        />
+                        <Button
+                          data-testid={ `${index}-card-name` }
+                          variant="info"
+                        >
+                          {recomendation.strMeal}
+                        </Button>
+                      </Link>
+                    </div>
+                  </div>
+                  <div
+                    key={ nextIndex() }
+                    data-testid={ `${nextIndex()}-recomendation-card` }
                   >
-                    {recomendation.strDrink}
-                  </h2>
-                </Link>
-              </div>
-            </div>
-          );
-        })}
+                    <div>
+                      <Link to={ `/comidas/${array[nextIndex()].idMeal}` }>
+                        <img
+                          src={ array[nextIndex()].strMealThumb }
+                          data-testid={ `${nextIndex()}-card-img` }
+                          alt="Meal"
+                          style={ { margin: '15px', width: '50%', borderRadius: '15px' } }
+                        />
+                        <Button
+                          data-testid={ `${nextIndex()}-card-name` }
+                          variant="info"
+                        >
+                          {array[nextIndex()].strMeal}
+                        </Button>
+                      </Link>
+                    </div>
+                  </div>
+                </Carousel.Item>
+              );
+            }
+            return (
+              <Carousel.Item key={ index }>
+                <div data-testid={ `${index}-recomendation-card` }>
+                  <div>
+                    <Link to={ `/comidas/${recomendation.idDrink}` }>
+                      <img
+                        src={ recomendation.strDrinkThumb }
+                        data-testid={ `${index}-card-img` }
+                        alt="Drink"
+                        style={ { margin: '15px', width: '50%', borderRadius: '15px' } }
+                      />
+                      <Button
+                        data-testid={ `${index}-card-name` }
+                        variant="info"
+                      >
+                        {recomendation.strDrink}
+                      </Button>
+                    </Link>
+                  </div>
+                </div>
+                <div
+                  key={ nextIndex() }
+                  data-testid={ `${nextIndex()}-recomendation-card` }
+                >
+                  <div>
+                    <Link
+                      to={ `/comidas/${array[nextIndex()].idDrink}` }
+                    >
+                      <img
+                        src={ array[nextIndex()].strDrinkThumb }
+                        data-testid={ `${nextIndex()}-card-img` }
+                        alt="Drink"
+                        style={ { margin: '15px', width: '50%', borderRadius: '15px' } }
+                      />
+                      <Button
+                        variant="info"
+                        data-testid={ `${nextIndex()}-card-name` }
+                      >
+                        {array[nextIndex()].strDrink}
+                      </Button>
+                    </Link>
+                  </div>
+                </div>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
       </div>
     );
   }
