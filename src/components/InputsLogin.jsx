1954f1bd5739bbbd1e21f40b8908eec00c0da22c
jvitorof <j.vitoroflores@gmail.com>
2021-02-06 17:13:24 -0300

    LISTCARDS: Bora

--- src/components/InputsLogin.jsx
+++ src/components/InputsLogin.jsx
@@ -1,30 +1,28 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Input, Button } from '@material-ui/core';
+// import { Input, Button } from '@material-ui/core';
 import { fetchApi, allFood } from '../services/fetchApi';
 import context from '../contextAPI/context';
 import '../css/login.css';
 
-const inputText = (onChange, login) => (
+const inputText = (onChange) => (
   <div className="input-email">
     <input
       data-testid="email-input"
       type="email"
       name="user"
-      value={ login.user }
       placeholder="email@email.com"
       onChange={ (e) => onChange(e) }
     />
   </div>
 );
 
-const inputPasswd = (onChange, login) => (
+const inputPasswd = (onChange) => (
   <div className="input-senha">
     <input
       data-testid="password-input"
       type="password"
       name="passwd"
-      value={ login.passwd }
       placeholder="sua senha aqui"
       onChange={ (e) => onChange(e) }
     />
@@ -78,8 +76,8 @@ export default function InputLogin() {
 
   return (
     <div className="input-login-controller">
-      {inputText(loginChanges, login)}
-      {inputPasswd(loginChanges, login)}
+      {inputText(loginChanges)}
+      {inputPasswd(loginChanges)}
       {buttonLogin(callRoute, isDisabled)}
     </div>
   );
