5dfdd506646478260e77d29a6cfb0db9e367d2f2
jvitorof <j.vitoroflores@gmail.com>
2021-02-06 20:58:48 -0300

    PROFILE: starting

--- src/App.js
+++ src/App.js
@@ -11,8 +11,40 @@ function App() {
       <Route exact path="/" component={ Login } />
       <Route path="/comidas" component={ Recipes } />
       <Route path="/bebidas" component={ Recipes } />
+      <Route path="/comidas/:id" />
+      <Route path="/bebidas/:id" />
+      <Route path="/comidas/:id/in-progress" />
+      <Route path="/bebidas/:id/in-progress" />
+      <Route path="/explorar" />
+      <Route path="/explorar/comidas" />
+      <Route path="/explorar/bebidas" />
+      <Route path="/explorar/comidas/ingredientes" />
+      <Route path="/explorar/bebidas/ingredientes" />
+      <Route path="/explorar/comidas/area" />
+      <Route path="/perfil" />
+      <Route path="/receitas-feitas" />
+      <Route path="/receitas-favoritas" />
     </Switch>
   );
 }
 
 export default App;
+
+/* 
+Tela de login: /;
+Tela principal de receitas de comidas: /comidas;
+Tela principal de receitas de bebidas: /bebidas;
+Tela de detalhes de uma receita de comida: /comidas/{id-da-receita};
+Tela de detalhes de uma receita de bebida: /bebidas/{id-da-receita};
+Tela de receita em processo de comida: /comidas/{id-da-receita}/in-progress;
+Tela de receita em processo de bebida: /bebidas/{id-da-receita}/in-progress;
+Tela de explorar: /explorar;
+Tela de explorar comidas: /explorar/comidas;
+Tela de explorar bebidas: /explorar/bebidas;
+Tela de explorar comidas por ingrediente: /explorar/comidas/ingredientes;
+Tela de explorar bebidas por ingrediente: /explorar/bebidas/ingredientes;
+Tela de explorar comidas por local de origem: /explorar/comidas/area;
+Tela de perfil: /perfil;
+Tela de receitas feitas: /receitas-feitas;
+Tela de receitas favoritas: /receitas-favoritas.
+*/
