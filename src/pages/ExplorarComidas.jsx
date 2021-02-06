9dee87939c5cb38ebd77ed688f0421cdaf99d690
MoisesSantana <moisaant@gmail.com>
2021-02-06 02:54:18 -0300

    Implementação dos botãos de explorar

--- src/pages/ExplorarComidas.jsx
+++ src/pages/ExplorarComidas.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { requestApiMealSurprise } from '../services/requestFood';
 
 function ExplorarComidas() {
+  const [mealSurpriseId, setMealSurpriseId] = useState('')
+
+  useEffect(() => {
+    const handleSurpriseEndpoint = async () => {
+      const endpoint = await requestApiMealSurprise();
+      setMealSurpriseId(endpoint[0].idMeal);
+    }
+    handleSurpriseEndpoint();
+  }, []);
+
+  if (!mealSurpriseId) return <span>Loading...</span>;
+
   return (
     <div>
       <Header name="Explorar Comidas" button={ false } />
+      <Link to="/explorar/comidas/ingredientes" data-testid="explore-by-ingredient">
+        Por Ingredientes
+      </Link>
+      <Link to="/explorar/comidas/area" data-testid="explore-by-area">
+        Por Local de Origem
+      </Link>
+      <Link
+        to={`/comidas/${mealSurpriseId}`}
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </Link>
       <Footer />
     </div>
   );
