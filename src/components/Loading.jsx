9317abee85d243bf39c2043d782522316d90bd84
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-02-09 00:12:11 -0300

    Finalizando linter

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,15 +3,13 @@ import { Spinner } from 'react-bootstrap';
 
 class Loading extends React.Component {
   render() {
-    return setTimeout(function(){
-      return (
-        <div style={ { height: '90vh', textAlign: 'center' } }>
-          <Spinner animation="border" size="xl" variant="success" style={ { margin: '220px' } }>
-            <p style={ { marginLeft: '60px' } }>Loading...</p>
-          </Spinner>
-        </div>
-      );
-    }, 2000);
+    return (
+      <div style={ { height: '90vh', textAlign: 'center' } }>
+        <Spinner animation="border" size="xl" variant="success" style={ { margin: '220px' } }>
+          <p style={ { marginLeft: '60px' } }>Loading...</p>
+        </Spinner>
+      </div>
+    );
   }
 }
 
