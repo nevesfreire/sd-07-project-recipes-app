caf91c7f595ac325fd8aef35481e7cd8a9d27e40
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2021-02-09 00:08:50 -0300

    Linter 💩

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,13 +3,15 @@ import { Spinner } from 'react-bootstrap';
 
 class Loading extends React.Component {
   render() {
-    return (
-      <div style={ { height: '90vh', textAlign: 'center' } }>
-        <Spinner animation="border" size="xl" variant="success">
-          <p style={ { marginLeft: '100px' } }>Loading...</p>
-        </Spinner>
-      </div>
-    );
+    return setTimeout(function(){
+      return (
+        <div style={ { height: '90vh', textAlign: 'center' } }>
+          <Spinner animation="border" size="xl" variant="success" style={ { margin: '220px' } }>
+            <p style={ { marginLeft: '60px' } }>Loading...</p>
+          </Spinner>
+        </div>
+      );
+    }, 2000);
   }
 }
 
