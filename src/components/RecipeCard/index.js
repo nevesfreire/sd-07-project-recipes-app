ee63aeaaf15c90503bb6e3f919f2fc05f053a556
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2021-02-08 22:47:29 -0300

    Ajustes Tentatva requisito 32

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -1,25 +1,35 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
 
 import StyledCard from './styles';
 
 const RecipeCard = (props) => {
   const [cardInfoState, setCardInfoState] = useState(props);
+  const { pathname } = useLocation();
+  const history = useHistory();
 
   useEffect(() => {
     setCardInfoState(props);
   }, [props]);
 
-  const { image, name, index } = cardInfoState.cardInfo;
+  const handleClick = (id) => history.push(
+    pathname.includes('comidas') ? `/comidas/${id}`
+      : `/bebidas/${id}`,
+  );
+
+  const { image, name, index, id } = cardInfoState.cardInfo;
   return (
     <StyledCard data-testid={ `${index}-recipe-card` }>
       <StyledCard.Img
         variant="top"
         src={ image }
         data-testid={ `${index}-card-img` }
+        onClick={ () => handleClick(id) }
       />
       <StyledCard.Body>
         <StyledCard.Title
           data-testid={ `${index}-card-name` }
+          onClick={ () => handleClick(id) }
         >
           {name}
         </StyledCard.Title>
