bb5a1c33ae1b7db26f64cd4bccf0a2d5ce2762b7
jvitorof <j.vitoroflores@gmail.com>
2021-02-18 00:10:00 -0300

    RECIPE LIST: Corrigido Toggle de Category

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -2,21 +2,44 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '@material-ui/core';
 import context from '../contextAPI/context';
-import { fetchApi, getFoodCategory, getDrinksCategory } from '../services/fetchApi';
+import {
+  fetchApi, getFoodCategory, getDrinksCategory, allFood, allDrink,
+} from '../services/fetchApi';
 
-const filterCategory = async (pathname, category, setState) => {
-  if (pathname === '/comidas') {
-    const newData = await fetchApi(getFoodCategory(category));
-    setState((s) => ({
-      ...s,
-      data: { ...s.data, food: newData.meals },
-    }));
-  } else if (pathname === '/bebidas') {
-    const newData = await fetchApi(getDrinksCategory(category));
-    setState((s) => ({
-      ...s,
-      data: { ...s.data, beverage: newData.drinks },
-    }));
+const filterCategory = async (pathname, category, state, setState) => {
+  if (category !== 'All' && category !== state.filtered) {
+    if (pathname === '/comidas') {
+      const newData = await fetchApi(getFoodCategory(category));
+      setState((s) => ({
+        ...s,
+        data: { ...s.data, food: newData.meals },
+        filtered: category,
+      }));
+    } else if (pathname === '/bebidas') {
+      const newData = await fetchApi(getDrinksCategory(category));
+      setState((s) => ({
+        ...s,
+        data: { ...s.data, beverage: newData.drinks },
+        filtered: category,
+      }));
+    }
+  }
+  if (category === 'All' || category === state.filtered) {
+    if (pathname === '/comidas') {
+      const newData = await fetchApi(allFood);
+      setState((s) => ({
+        ...s,
+        data: { ...s.data, food: newData.meals },
+        filtered: '',
+      }));
+    } else if (pathname === '/bebidas') {
+      const newData = await fetchApi(allDrink);
+      setState((s) => ({
+        ...s,
+        data: { ...s.data, beverage: newData.drinks },
+        filtered: '',
+      }));
+    }
   }
 };
 
@@ -26,14 +49,14 @@ const CategoryButton = (props) => {
     pathname,
   } = props;
 
-  const { setState } = useContext(context);
+  const { state, setState } = useContext(context);
 
   return (
     <Button
       variant="contained"
       data-testid={ `${category}-category-filter` }
       type="button"
-      onClick={ () => filterCategory(pathname, category, setState) }
+      onClick={ () => filterCategory(pathname, category, state, setState) }
       className="category-button"
     >
       {category}
