03952a18638e81cc1d7c80ecf4d3a46fd4ea7294
ygorvmdf <ygorvitor.fonseca@gmail.com>
2021-02-21 21:56:53 -0300

    favorite recipes

--- src/__test__/favorite.recipes.test.js
+++ src/__test__/favorite.recipes.test.js
@@ -6,7 +6,7 @@ import fetchMock from './mocks/fetch';
 
 global.fetch = jest.fn().mockImplementation(fetchMock);
 
-localStorage.setItem('doneRecipes', JSON.stringify([{ alcoholicOrNot: '',
+localStorage.setItem('favoriteRecipes', JSON.stringify([{ alcoholicOrNot: '',
   area: 'Turkish',
   category: 'Side',
   doneDate: 1613068818732,
@@ -30,7 +30,7 @@ localStorage.setItem('doneRecipes', JSON.stringify([{ alcoholicOrNot: '',
 
 describe('implement all the data-testids', () => {
   it('should render all the data-testid', () => {
-    renderWithRouter(<App />, { route: '/receitas-feitas' });
+    renderWithRouter(<App />, { route: '/receitas-favoritas' });
 
     expect(screen.getByTestId('filter-by-all-btn')).toBeInTheDocument();
     expect(screen.getByTestId('filter-by-food-btn')).toBeInTheDocument();
@@ -38,21 +38,19 @@ describe('implement all the data-testids', () => {
     expect(screen.getByTestId('0-horizontal-image')).toBeInTheDocument();
     expect(screen.getByTestId('0-horizontal-top-text')).toBeInTheDocument();
     expect(screen.getByTestId('0-horizontal-name')).toBeInTheDocument();
-    expect(screen.getByTestId('0-horizontal-done-date')).toBeInTheDocument();
     expect(screen.getByTestId('0-horizontal-share-btn')).toBeInTheDocument();
-    expect(screen.getAllByTestId('0-undefined-horizontal-tag')[0]).toBeInTheDocument();
+    expect(screen.getByTestId('0-horizontal-favorite-btn')).toBeInTheDocument();
   });
 });
 
 describe('the card should contain the corrects elements', () => {
-  it('should render the corrects elemente for a food recipe', () => {
-    renderWithRouter(<App />, { route: '/receitas-feitas' });
+  it('should render the corrects element for a food recipe', () => {
+    renderWithRouter(<App />, { route: '/receitas-favoritas' });
 
     expect(screen.getByTestId('0-horizontal-image')).toBeInTheDocument();
     expect(screen.getByTestId('0-horizontal-top-text')).toBeInTheDocument();
     expect(screen.getByTestId('0-horizontal-name')).toBeInTheDocument();
-    expect(screen.getByTestId('0-horizontal-done-date')).toBeInTheDocument();
     expect(screen.getByTestId('0-horizontal-share-btn')).toBeInTheDocument();
-    expect(screen.getAllByTestId('0-undefined-horizontal-tag')[0]).toBeInTheDocument();
+    expect(screen.getByTestId('0-horizontal-favorite-btn')).toBeInTheDocument();
   });
 });
